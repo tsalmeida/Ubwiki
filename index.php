6aa3db8da38584ac429e619424ff26c1d8fbf3c6
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-07 00:13:59 +0100

    Páginas básicas
    Páginas básicas, início de trabalhos

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<?php
-
-echo "<html>
-  <h1>Título</h1>
-  <p>
-    Teste de caneta
-  </p>
-</html>"
-
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1">
+		<meta charset="UTF-8">
+		<link rel='stylesheet' href='stylesheet.css'/>
+		<link rel='icon' type='image/vnd.microsoft.icon' href='../nxst/favicons/simple.scss.ico'/>
+		<script src="https://use.fontawesome.com/bc556cc56a.js"></script>
+		<title>Ubwiki</title>
+	</head>
+	<body>
+        <h1>Ubwiki</h1>
+        <p>Central de estudos para o CACD</p>
+	</body>
+</html>
