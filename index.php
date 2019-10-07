ca8aefc76eef3bd8a1695dde3150fa7548aa4eaa
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-07 01:45:23 +0100

    Update index.php
    bootstrap

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Ubwiki!</title>
   </head>
@@ -17,7 +17,7 @@
 
     <div class="jumbotron text-center">
       <h1>Ubwiki</h1>
-      <p>Ambiente de estudos da Ubique</p>
+      <p>Ambiente de estudos da Ubique.</p>
     </div>
 
     <div class="container">
