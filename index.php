d2485fa6f092de392b297824a5d3e7bc77ac40ac
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-07 19:27:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,67 +13,8 @@
 
     <title>Ubwiki!</title>
 
-    <style>
-        /* Remove the navbar's default margin-bottom and rounded borders */
-        .navbar {
-          margin-bottom: 0;
-          border-radius: 0;
-        }
-
-        /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-        .row.content {height: 450px}
-
-        /* Set gray background color and 100% height */
-        .sidenav {
-          padding-top: 20px;
-          background-color: #f1f1f1;
-          height: 100%;
-        }
-
-        /* Set black background color, white text and some padding */
-        footer {
-          background-color: #555;
-          color: white;
-          padding: 15px;
-          bottom: 0;
-          position: absolute;
-        }
-
-        /* On small screens, set height to 'auto' for sidenav and grid */
-        @media screen and (max-width: 767px) {
-          .sidenav {
-            height: auto;
-            padding: 15px;
-          }
-          .row.content {height:auto;}
-        }
-      </style>
-
   </head>
   <body>
-
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Logo</a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#about">Ubique</a></li>
-            <li><a href="#services">Ambiente de Estudos</a></li>
-            <li><a href="#portfolio">Ubwiki</a></li>
-            <li><a href="#pricing">Acervo</a></li>
-            <li><a href="#contact">Praeterea</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
     <div class="container-fluid text-center">
       <div class="row content">
         <div class="col-sm-10 text-left">
