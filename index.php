b4024029e6bf33d5552b5d04cb80e4df6dd329c7
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-07 09:03:33 +0100

    Update index.php
    Starting with page.

--- index.php
+++ index.php
@@ -84,18 +84,12 @@
           <p><a href="#">Link</a></p>
         </div>
         <div class="col-sm-8 text-left">
-          <h1>Welcome</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <hr>
-          <h3>Test</h3>
-          <p>Lorem ipsum...</p>
+          <h1>Ubwiki</h1>
+          <p>Superior ordem em seus estudos para o CACD.</p>
         </div>
         <div class="col-sm-2 sidenav">
           <div class="well">
-            <p>ADS</p>
-          </div>
-          <div class="well">
-            <p>ADS</p>
+            <p>A Ubwiki atua em conjunção com o acervo de aulas e ferramentas da Ubique, especialmente o Ambiente de Estudos Ubique.</p>
           </div>
         </div>
       </div>
