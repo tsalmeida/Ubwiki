33a7fe82f4da0cd6c8d80c4194307043b85ce44e
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-08 12:17:51 +0100

    Update termos.php

--- termos.php
+++ termos.php
@@ -4,14 +4,11 @@ top_page();
 ?>
   <body>
 
-    <div class="px-1 py-1">
+    <div class="px-1 py-1 col-sm-12">
         <div class="jumbotron col-sm-12 mb-0">
           <h1 class="display-4">Ubwiki</h1>
         </div>
     </div>
-    <!-- <div class="embed-responsive embed-responsive-16by9">
-      <iframe class="embed-responsive-item" src="https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true" allowfullscreen></iframe>
-    </div> -->
     <?php
       echo file_get_contents("https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true");
      ?>
