c1863d92ff178ad34729f833e9a7067aee1977c1
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-08 18:15:31 +0100

    Update termos.php

--- termos.php
+++ termos.php
@@ -11,17 +11,20 @@ top_page();
     <?php
       // $gdoc = file_get_contents("https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true");
 
-      $ch = curl_init("https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true");
+      <?php
+      // create a new cURL resource
+      $ch = curl_init();
 
-      $doc = false;
-
+      // set URL and other appropriate options
+      curl_setopt($ch, CURLOPT_URL, "ttps://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true");
       curl_setopt($ch, CURLOPT_HEADER, false);
-      curl_setopt($ch, CURLOPT_FILE, $doc);
 
+      // grab URL and pass it to the browser
       curl_exec($ch);
-      curl_close($ch);
 
-      echo $doc;
+      // close cURL resource, and free up system resources
+      curl_close($ch);
+      ?>
 
      ?>
   </body>
