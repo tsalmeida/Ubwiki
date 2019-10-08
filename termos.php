6dc88f01bf19ac9c1c2d7dd48340ffbb7f022a91
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-08 12:41:41 +0100

    Update termos.php

--- termos.php
+++ termos.php
@@ -3,10 +3,12 @@ include 'engine.php';
 top_page();
 ?>
   <body>
-    <div class="px-1 py-1">
+    <div class="container px-1 py-1 text-center">
+      <div class="row text-center">
         <div class="jumbotron col-sm-12 mb-0">
           <h1 class="display-4">Ubwiki</h1>
         </div>
+    	</div>
     </div>
     <?php
       echo file_get_contents("https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true");
