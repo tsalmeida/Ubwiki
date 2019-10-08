bf062e292ae75c43036631e38871fb3c3a48b1e9
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-08 09:54:28 +0100

    php functions for loading pages

--- termos.php
+++ termos.php
@@ -1,18 +1,7 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- General stuff -->
-    <link type='image/vnd.microsoft.icon' rel='icon' href='../nxst/favicons/simple.scss.ico'/>
-    <link type="text/css" rel='stylesheet' href='ubiquestyle.css'/>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="bootstrap.min.css">
-
-    <title>Ubwiki - Termos de Uso</title>
-  </head>
+<?php
+include 'engine.php';
+top_page();
+?>
   <body>
 
     <div class="jumbotron">
@@ -22,11 +11,8 @@
     <div class="embed-responsive embed-responsive-16by9">
       <iframe class="embed-responsive-item" src="https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true" allowfullscreen></iframe>
     </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
+  <?php
+    bottom_page();
+  ?>
 </html>
