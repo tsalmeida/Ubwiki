9efbc1f2da4d37d84ae09c58f83a6190a7e9e065
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-08 16:56:00 +0100

    Update termos.php

--- termos.php
+++ termos.php
@@ -10,9 +10,9 @@ top_page();
     </div>
     <iframe>
     <?php
-      $gdoc = file_get_contents("https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true");
       $result = _isCurl();
-      echo "<p>$result</p>";
+      echo "<h1>$result</h1>";
+      $gdoc = file_get_contents("https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true");
      ?>
     </iframe>
   </body>
