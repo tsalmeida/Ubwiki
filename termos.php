ee2e5922acbaa8f5999033548483c6a26b2c12d8
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-08 18:48:42 +0100

    Update termos.php

--- termos.php
+++ termos.php
@@ -14,8 +14,8 @@ top_page();
     $ch = curl_init();
 
     // set URL and other appropriate options
+    curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_URL, "https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true");
-    curl_setopt($ch, CURLOPT_HEADER, false);
 
     // grab URL and pass it to the browser
     curl_exec($ch);
