1020e7640fcb8e7a525057357f65a7bcf4f6d7af
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-08 11:46:59 +0100

    Changes

--- termos.php
+++ termos.php
@@ -10,7 +10,7 @@ top_page();
         </div>
     </div>
     <div class="embed-responsive embed-responsive-16by9">
-      <iframe class="embed-responsive-item" src="https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true" allowfullscreen></iframe>
+      <iframe class="embed-responsive-item center-gdoc" src="https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true" allowfullscreen></iframe>
     </div>
   </body>
   <?php
