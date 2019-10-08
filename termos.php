0c9d3ced140f8ec32cc86f7c184d13d18f1d6a87
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-08 13:01:44 +0100

    Update termos.php

--- termos.php
+++ termos.php
@@ -3,17 +3,16 @@ include 'engine.php';
 top_page();
 ?>
   <body>
-    <div class="container px-1 py-1 text-center">
-      <div class="row text-center">
+    <div class="row px-1 py-1 text-center">
         <div class="jumbotron col-sm-12 mb-0">
           <h1 class="display-4">Ubwiki</h1>
         </div>
-    	</div>
     </div>
+    <iframe>
     <?php
       $gdoc = file_get_contents("https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true");
-      error_log($gdoc);
      ?>
+    </iframe>
   </body>
   <?php
     bottom_page();
