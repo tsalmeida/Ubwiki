0e23c28c3c2a14f504de42fccd7f9cb30bb43f84
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-08 09:33:25 +0100

    php functions

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- General stuff -->
-    <link type='image/vnd.microsoft.icon' rel='icon' href='../nxst/favicons/simple.scss.ico'/>
-    <link type="text/css" rel='stylesheet' href='ubiquestyle.css'/>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Ubwiki</title>
-  </head>
+<?php
+include 'engine.php';
+top_page();
+?>
   <body>
 
     <div class="row justify-content-center">
@@ -111,12 +100,8 @@
     <footer class="container-fluid text-center bg-secondary text-white">
       <p>A Ubwiki é uma ferramenta de uso público e gratuito. Todos os direitos são reservados ao Grupo Ubique. Clique <a href="termos.php" target="_blank">aqui</a> para rever os termos e condições de uso da página.</p>
     </footer>
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<?php
+  bottom_page();
+?>
   </body>
 </html>
