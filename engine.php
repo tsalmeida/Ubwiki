81de11b8e9920e0391a20466562997f9a1261624
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-09 14:45:55 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -59,8 +59,11 @@ function extract_zoho($linkplanilha, $authtoken, $ownername, $materia, $scope) {
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $output = curl_exec($ch);
   curl_close($ch);
-  // $output = json_decode($output, TRUE);
-  // $output = serialize($output);
+
+  $xml = simplexml_load_string($output, "SimpleXMLElement", LIBXML_NOCDATA);
+  $json = json_encode($xml);
+  $array = json_decode($json,TRUE);
+  $output = serialize($array);
   return $output;
 }
 
