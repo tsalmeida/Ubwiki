e0eb6eb13c9d1b44e815db06d3ac7517aab467ba
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-11 00:41:01 +0100

    Create userpage.php

---
+++ userpage.php
@@ -0,0 +1,24 @@
+  <?php
+  include 'engine.php';
+  top_page();
+  ?>
+  <body>
+    <div class="container-fluid px-1 py-1 mb-3 text-center">
+      <div class="jumbotron col-sm-12 mb-0">
+        <h1 class="display-4 logo-jumbotron">Ubwiki</h1>
+      </div>
+    </div>
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+          <h1>Sua página</h1>
+          <p>Informações do usuário aqui. Verbetes em que colaborou, comentários que escreveu, temas em que já fez anotações, temas prioritários etc.</p>
+        </div>
+        <div class="col-sm-2"></div>
+      </div>
+    </div>
+  </body>
+  <?php
+    bottom_page();
+  ?>
