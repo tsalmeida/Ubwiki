d3737bbaf1ac5cd4a3088858e8b6a4cfe81fa244
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-11 16:51:40 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -147,7 +147,7 @@ if (isset($_POST['searchBarGo'])) {
   $concurso = $_POST['searchBarGo'];
   $command = $_POST['searchbar'];
   error_log("this happened: concurso: $concurso and command: $command");
-  header("materias.php?sigla=PICACD&concurso=CACD");
+  headers("materias.php?sigla=PICACD&concurso=CACD");
   // $servername = "localhost";
   // $username = "grupoubique";
   // $password = "ubique patriae memor";
