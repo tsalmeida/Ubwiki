df3dd922bd3aea2ca5f87172f2ad2926c3902559
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-11 16:30:11 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -146,21 +146,22 @@ function standard_jumbotron() {
 if (isset($_POST['searchBarGo'])) {
   $concurso = $_POST['searchBarGo'];
   $command = $_POST['searchbar'];
-  error_log("this happened: concurso: $concurso and command: $command");
-  $servername = "localhost";
-  $username = "grupoubique";
-  $password = "ubique patriae memor";
-  $dbname = "Ubique";
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  mysqli_set_charset($conn,"utf8");
-  $result = $conn->query("SELECT sigla FROM Materias WHERE concurso = '$concurso' AND estado = 1 AND materia = '$command' ORDER BY ordem");
-  if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      $sigla = $row["sigla"];
-      header("materias.php?sigla=$sigla&concurso=$concurso");
-    }
-  }
-  $conn->close();
+  // error_log("this happened: concurso: $concurso and command: $command");
+  header("materias.php?sigla=PICACD&concurso=CACD");
+  // $servername = "localhost";
+  // $username = "grupoubique";
+  // $password = "ubique patriae memor";
+  // $dbname = "Ubique";
+  // $conn = new mysqli($servername, $username, $password, $dbname);
+  // mysqli_set_charset($conn,"utf8");
+  // $result = $conn->query("SELECT sigla FROM Materias WHERE concurso = '$concurso' AND estado = 1 AND materia = '$command' ORDER BY ordem");
+  // if ($result->num_rows > 0) {
+  //   while($row = $result->fetch_assoc()) {
+  //     $sigla = $row["sigla"];
+  //     header("materias.php?sigla=$sigla&concurso=$concurso");
+  //   }
+  // }
+  // $conn->close();
 }
 
 function carregar_pagina($sigla, $concurso) {
