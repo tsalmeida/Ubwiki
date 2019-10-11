06c3373973d5c83314457c70f2a29f6d18c46c15
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-11 11:52:03 +0100

    reverting to last night

--- engine.php
+++ engine.php
@@ -1,10 +1,8 @@
 <?php
-
 function top_page() {
   echo '
   <!DOCTYPE html>
   <html lang="en">
-
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -17,13 +15,11 @@ function top_page() {
     <link href="css/mdb.min.css" rel="stylesheet">
     <!-- Your custom styles (optional) -->
     <link href="css/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="ubiquestyle.css">
     <link type="image/vnd.microsoft.icon" rel="icon" href="../nxst/favicons/simple.scss.ico"/>
     <title>Ubwiki</title>
   </head>
   ';
 }
-
 function bottom_page() {
   echo '
   <!-- JQuery -->
@@ -37,7 +33,6 @@ function bottom_page() {
   </html>
   ';
 }
-
 function extract_gdoc($url) {
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
@@ -50,7 +45,6 @@ function extract_gdoc($url) {
   curl_close($ch);
   return $body;
 }
-
 function extract_zoho($linkplanilha, $authtoken, $ownername, $materia, $scope) {
   $ch = curl_init();
   $linkplanilha = "$linkplanilha?authtoken=$authtoken&zc_ownername=$ownername&materia=$materia&scope=$scope";
@@ -59,19 +53,15 @@ function extract_zoho($linkplanilha, $authtoken, $ownername, $materia, $scope) {
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $output = curl_exec($ch);
   curl_close($ch);
-
   $xml = simplexml_load_string($output, "SimpleXMLElement", LIBXML_NOCDATA);
   $json = json_encode($xml);
   $array = json_decode($json,TRUE);
   $output = serialize($array);
   return $output;
 }
-
 function connect_to_mysql($id, $materia) {
-
   return $result;
 }
-
 function ler_cartoes($concurso) {
   $servername = "localhost";
   $username = "grupoubique";
@@ -79,34 +69,29 @@ function ler_cartoes($concurso) {
   $dbname = "Ubique";
   $conn = new mysqli($servername, $username, $password, $dbname);
   mysqli_set_charset($conn,"utf8");
-  $result = $conn->query("SELECT sigla, materia, ordem, concurso FROM Materias ORDER BY ordem");
+  $result = $conn->query("SELECT sigla, materia, ordem, concurso, estado FROM Materias ORDER BY ordem");
   if ($result->num_rows > 0) {
-    while(($row = $result->fetch_assoc()) && ($row['concurso'] = $concurso)) {
+    $count = 1;
+    while(($row = $result->fetch_assoc()) && ($row['concurso'] = $concurso) && ($row['estado'] != false)) {
+      if ($count == 1) { echo "<div class='row justify-content-center'>"; }
+      $count++;
       $sigla = $row["sigla"];
       $materia = $row["materia"];
       echo "
-        <div class='col-lg-1 col-md-3 py-3 px-3'>
-          <a href='#verbetes' class=''>
-            <div class='card card-cascade narrower'>
-              <div class='view view-cascade overlay'>
-                <img src='imagens/$sigla.jpg' class='card-img-top rounded-circle img-fluid zoom'
-                  alt='$materia'>
-                <a>
-                  <div class='mask rgba-white-slight'></div>
-                </a>
-              </div>
-              <div class='card-body card-body-cascade'>
-                <a href='#verbetes' class=''><small class='text-muted'>$materia</small></a>
-              </div>
-            </div>
-          </a>
+      <a href='#verbetes'>
+        <div class='col-lg-2 bg-lighter mx-3 my-3 py-0 px-1 rounded bdark'>
+          <a href='#verbetes'><small class='text-muted text-uppercase smaller'>$materia</small></a>
         </div>
+      </a>
       ";
+      if ($count == 5) {
+        echo "</div>";
+        $count = 1;
+      }
     }
   }
   $conn->close();
 }
-
 function ler_edital($materia) {
   $servername = "localhost";
   $username = "grupoubique";
@@ -116,7 +101,6 @@ function ler_edital($materia) {
   mysqli_set_charset($conn,"utf8");
   $sql = "SELECT etiqueta, materia, superior FROM Etiquetas";
   $result = $conn->query($sql);
-
   if ($result->num_rows > 0) {
       while($row = $result->fetch_assoc()) {
         $check = $row['materia'];
@@ -129,29 +113,29 @@ function ler_edital($materia) {
   }
   $conn->close();
 }
-
-if (isset($_POST['searchbar'])) {
-	$command = $_POST['searchbar'];
-  $concurso = $_POST['searchbar'];
+function readSearchOptions($concurso) {
   $servername = "localhost";
   $username = "grupoubique";
   $password = "ubique patriae memor";
   $dbname = "Ubique";
   $conn = new mysqli($servername, $username, $password, $dbname);
   mysqli_set_charset($conn,"utf8");
-  $result = $conn->query("SELECT materia, ordem, concurso FROM Materias ORDER BY ordem");
+  $result = $conn->query("SELECT materia, ordem, concurso, estado FROM Materias ORDER BY ordem");
   if ($result->num_rows > 0) {
-    while(($row = $result->fetch_assoc()) && ($row['concurso'] = $concurso)) {
-
+    while(($row = $result->fetch_assoc()) && ($row['concurso'] = $concurso) && ($row['estado'] != false)) {
+      $materia = $row["materia"];
+      echo "<option>$materia</option>";
     }
   }
   $conn->close();
-  return;
 }
-
-if (isset($_POST['searchbar'])) {
-  $termo = $_POST['searchbar'];
-  $termo = mb_strtolower($termo);
+function standard_jumbotron() {
+  echo "
+  <div class='container-fluid px-1 py-1 mb-3 text-center'>
+    <div class='jumbotron col-sm-12 mb-0'>
+      <a href='index.php'><h1 class='display-4 logo-jumbotron'>Ubwiki</h1></a>
+    </div>
+  </div>
+  ";
 }
-
 ?>
