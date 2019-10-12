b7a1b46ee309a0f829589ec2cf39158706de8ec1
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-12 18:56:08 +0100

    Update verbete.php

--- verbete.php
+++ verbete.php
@@ -24,7 +24,9 @@ if (isset($_GET['concurso'])) {
       <div class="row">
         <div class="col-sm-2"></div>
         <div class="col-sm-8">
-          <?php carregar_verbete($tema, $concurso); ?>
+          <?php
+            echo "<p>$tema</p>";
+          carregar_verbete($tema, $concurso); ?>
         </div>
         <div class="col-sm-2"></div>
       </div>
