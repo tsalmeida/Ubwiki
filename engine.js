f1483db41cec72e7b1e3cc937bbbf4642483931f
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-12 13:47:21 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 		$.post('engine.php', {'sbcommand': command, 'sbconcurso': concurso}, function(data) {
 			$("#searchBar").val('');
 			if (data != 0) {
-        alert('this happened');
+        alert(data);
 				var pw = data.substring(0, 16);
 				var pw2 = data.substring(16);
 				if (pw == 'notfoundnotfound') {
