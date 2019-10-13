a8f73c3778a70a05764a9e2a764e857566ec8729
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-13 12:54:16 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -14,7 +14,10 @@
           <form class='text-center border border-light p-5'>
                 <p class="h4 mb-4">Funções</p>
                 <p>Reconstruir tabela de opções de barra de busca.</p>
-                <button class="btn btn-info btn-block my-4" type='submit'>Reconstruir</button>
+                <input type="text" id="reconstruir_concurso" class="from-control mb-4" placeholder="Concurso"></input>
+                <?php
+                echo "<button id='reconstruir' class='btn btn-info btn-block my-4' type='submit' value='$concurso'>Reconstruir</button>";
+                ?>
           </form>
         </div>
         <div class="col-sm-2"></div>
