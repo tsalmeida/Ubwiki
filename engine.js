71bdd95c3c29fe56ed734f05f979c0da3679faa5
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-13 21:03:52 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -27,25 +27,5 @@ $(document).ready(function() {
     });
     return false;
 		// comment
-  });$('#editar_verbete').click(function() {
-    var command = $('#searchBar').val();
-    var command = btoa(command);
-    var concurso = $('#searchBarGo').val();
-    var concurso = btoa(concurso);
-    $.post('engine.php', {'sbcommand': command, 'sbconcurso': concurso}, function(data) {
-      $("#searchBar").val('');
-      if (data != 0) {
-        var pw = data.substring(0, 16);
-        var pw2 = data.substring(16);
-        if (pw == 'notfoundnotfound') {
-          $("#searchBar").val(pw2);
-        }
-        else if (pw = 'foundfoundfoundf') {
-          window.open(pw2, '_self');
-        }
-      }
-    });
-    return false;
-		// comment
   });
 });
