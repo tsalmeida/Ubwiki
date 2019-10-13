9534a1a05986ea7edf827b69a832b1c380e8cc3c
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-13 15:45:11 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -14,7 +14,23 @@
           <form class='text-center border border-light p-5' method='post'>
               <p class="h4 mb-4">Funções</p>
               <p>Reconstruir tabela de opções de barra de busca.</p>
-              <input type="text" name="reconstruir_concurso" class="from-control mb-4" placeholder="Concurso"></input>
+              <fieldset class="form-group">
+                <div class="row">
+                  <legend class="col-form-label col-sm-2 pt-0">Concurso</legend>
+                  <div class="col-sm-10">
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="reconstruir_concurso" value="CACD" checked>
+                      <label class="form-check-label" for="gridRadios1">
+                        CACD
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </fieldset>
+
+
+
+
               <?php
               echo "<button class='btn btn-info btn-block my-4' type='submit'>Reconstruir</button>";
               ?>
