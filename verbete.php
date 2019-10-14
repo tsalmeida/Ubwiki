92957263bfba11134eb74e99b85214ff30190704
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-14 06:41:14 +0100

    edicao de verbetes

--- verbete.php
+++ verbete.php
@@ -24,7 +24,7 @@ if (isset($_GET['concurso'])) {
       <div class="row">
         <div class="col-sm-2"></div>
         <div class="col-sm-8">
-          <?php carregar_verbete($id_tema, $concurso); ?>
+          <?php carregar_verbete($tema, $concurso); ?>
         </div>
         <div class="col-sm-2"></div>
       </div>
