e6d769c618fe112bfec42e780033211f0eb0fe45
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-14 07:29:22 +0100

    edicao temas

--- admin.php
+++ admin.php
@@ -29,6 +29,26 @@
               </fieldset>
             <button class='btn btn-info btn-block my-4' type='submit'>Acessar ferramenta</button>
           </form>
+          <form class='text-center border border-light p-5' method='post'>
+              <p class="h4 mb-4">Funções</p>
+              <p>Reconstruir tabela de opções da barra de busca.</p>
+              <fieldset class="form-group">
+                <div class="row">
+                  <legend class="col-form-label col-sm-2 pt-0">Concurso</legend>
+                  <div class="col-sm-10">
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="reconstruir_concurso" value="CACD" checked>
+                      <label class="form-check-label" for="gridRadios1">
+                        CACD
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </fieldset>
+            <?php
+            echo "<button class='btn btn-info btn-block my-4' type='submit'>Reconstruir</button>";
+            ?>
+          </form>
         </div>
         <div class="col-sm-2"></div>
       </div>
