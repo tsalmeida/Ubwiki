9f062f8fddf508b00ff9470d0bced06cf83f811d
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-14 09:05:14 +0100

    Update edicao_temas.php

--- edicao_temas.php
+++ edicao_temas.php
@@ -19,10 +19,10 @@
           <form class='text-center border border-light p-5' method='post'>
               <p class="h4 mb-4">Tabela de temas: metalinguagem e organização</p>
               <p class='text-left'>Utiliza-se esta ferramenta para alterar a estrutura de temas do edital dos concursos. Alguns temas do edital são muito vagos, exigindo que sejam criadas novas sub-entradas. Outros incluem vários temas ao mesmo tempo, precisando ser divididos em várias entradas. É necessário, ainda, determinar uma metalinguagem permanente, que seja independente do texto do edital, que será reconhecida pelo sistema como correspondente àquele tema.</p>
+              <?php
+                carregar_edicao_temas($concurso);
+               ?>
           </form>
-          <?php
-            carregar_edicao_temas($concurso);
-           ?>
         </div>
         <div class="col-sm-2"></div>
       </div>
