8fa8185c91b8efa5c1a197dbeafc758edb8a9b9e
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-15 18:52:23 +0100

    Update verbete.php

--- verbete.php
+++ verbete.php
@@ -71,10 +71,10 @@ if (isset($_GET['concurso'])) {
       <div class='col-lg-2 text-center'>
         <span class='align-middle'>Verbete consolidado</span>
       </div>
-      <div class='col-lg-1 text-center'>
+      <div class='col-lg-1 text-center'></div>
+      <div class='col-lg-2'>
         <?php echo "<div class='col-lg-1 h2 float-right'><a href='editar_verbete.php?concurso=$concurso&tema=$id_tema'><i class='fal fa-edit'></i></a></div>"; ?>
       </div>
-      <div class='col-lg-2'></div>
       <div class='col-lg-6 text-left'>
         <?php
           if ($verbete == false) {
