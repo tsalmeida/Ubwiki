3fe789e1f2727529654fe821543cd5232cbb6f4b
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-15 11:29:57 +0100

    Update verbete.php

--- verbete.php
+++ verbete.php
@@ -65,7 +65,7 @@ if (isset($_GET['concurso'])) {
         <div class='col-lg-2'>
           <div class='container-fluid mb-5 py-2 bg-lighter rounded'>
             <div class='row'>
-              <h4>Índice</h4>
+              <h4 class='text-center'>Índice</h4>
             </div>
             <div class='row mt-2'>
               <div class='col-lg-12'>
@@ -96,7 +96,7 @@ if (isset($_GET['concurso'])) {
               <div class='col-lg-12'>
 <?php
               if ($verbete == false) {
-                echo "<div class='col-lg-12 bg-white'><p>Ainda não há verbete consolidado para este tema.</p></div></div></div>";
+                echo "<div class='col-lg-12 bg-white'><p>Ainda não há verbete consolidado para este tema.</p></div></div></div></div>";
               }
               else {
                 $verbete = base64_decode($verbete);
@@ -107,7 +107,7 @@ if (isset($_GET['concurso'])) {
                     echo "<p>$line</p>";
                     $line = strtok( $separator );
                 }
-                echo "</div></div>";
+                echo "</div></div></div>";
               }
 ?>
           </div>
