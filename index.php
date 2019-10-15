459082aa74d32d0a9253b9222d6614936077211c
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-15 22:11:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,17 +7,17 @@ $concurso = "CACD";
     <?php carregar_navbar(); ?>
     <div class="container-fluid text-center bg-white justify-content-center home1">
       <div class="row justify-content-center">
-          <div class="col-lg-2">
+          <div class="col-sm-2">
               <img class="img-fluid logo" src="imagens/ubiquelogo.png"></img>
           </div>
       </div>
       <div class="row justify-concent-center">
-        <div class="col-lg-12">
+        <div class="col-sm-12">
           <?php echo "<p class='lead'>Bem-vindo à Ubwiki, o sistema mais inteligente de preparação para o $concurso.</p>"; ?>
         </div>
       </div>
       <div class="row justify-content-center">
-        <div class="col-lg-6">
+        <div class="col-sm-6">
           <form id="searchform" action="" method="post">
             <div id="searchDiv">
               <input id="searchBar" list="searchlist" type="text" class="searchBar" name="searchBar" rows="1" autocomplete="off" spellcheck="false" placeholder="O que você vai estudar hoje?" required></input>
@@ -36,7 +36,7 @@ $concurso = "CACD";
     </div>
     <div class="container-fluid text-center bg-white home2 pt-5">
       <div class="row justify-content-center">
-        <div class="col-lg-10">
+        <div class="col-sm-10">
             <?php
                 ler_cartoes($concurso, 4);
             ?>
