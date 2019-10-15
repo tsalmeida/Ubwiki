7d28f9cf97fad63c8c20bfe3e049b86fa6dc6595
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-15 04:35:06 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -683,7 +683,7 @@ if (isset($_POST['metatemas_automaticos'])) {
       $novo_metaid = str_replace(")", "", $novo_metaid);
       $novo_metaid = str_replace("ó", "o", $novo_metaid);
       $novo_metaid = str_replace("ô", "o", $novo_metaid);
-      $novo_metaid = utf_decode($novo_metaid);
+      $novo_metaid = utf8_decode($novo_metaid);
       $novo_metaid = substr($novo_metaid, 0, 30);
       $novo_metaid = utf8_encode($novo_metaid);
       $update = $conn->query("UPDATE Temas SET metaid = '$novo_metaid' WHERE id = '$id'");
