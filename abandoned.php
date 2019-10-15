e1e7e02760d9f432ba5a8ce99c5948518535fcc4
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-15 05:04:23 +0100

    edicao de verbetes

--- abandoned.php
+++ abandoned.php
@@ -1,9 +1,5 @@
-<?php
-
-if (isset($_POST['sbcommand'])) {
-  $concurso = base64_decode($_POST['sbconcurso']);
-  $command = base64_decode($_POST['sbcommand']);
-  $command = utf8_encode($command);
+if (isset($_POST['metatemas_automaticos'])) {
+  $concurso = $_POST['metatemas_automaticos'];
   $servername = "localhost";
   $username = "grupoubique";
   $password = "ubique patriae memor";
@@ -11,115 +7,97 @@ if (isset($_POST['sbcommand'])) {
   $found = false;
   $conn = new mysqli($servername, $username, $password, $dbname);
   mysqli_set_charset($conn,"utf8");
-  $result = $conn->query("SELECT sigla FROM Materias WHERE concurso = '$concurso' AND estado = 1 AND materia = '$command' ORDER BY ordem");
+  $result = $conn->query("SELECT id, nivel1, nivel2, nivel3, nivel4, nivel5 FROM Temas WHERE concurso = '$concurso' AND metaid IS NULL");
   if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-      $sigla = $row["sigla"];
-      echo "foundfoundfoundfmateria.php?sigla=$sigla&concurso=$concurso";
-      $conn->close();
-      $found = true;
+      $id = $row['id'];
+      $nivel1 = $row['nivel1'];
+      $nivel2 = $row['nivel2'];
+      $nivel3 = $row['nivel3'];
+      $nivel4 = $row['nivel4'];
+      $nivel5 = $row['nivel5'];
+      if ($nivel5 != false) { $novo_metaid = $nivel5; }
+      elseif ($nivel4 != false) { $novo_metaid = $nivel4; }
+      elseif ($nivel3 != false) { $novo_metaid = $nivel3; }
+      elseif ($nivel2 != false) { $novo_metaid = $nivel2; }
+      else { $novo_metaid = $nivel1; }
+      $novo_metaid = strtolower($novo_metaid);
+      $novo_metaid = str_replace(" e ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" a ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" o ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" as ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" os ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" de ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" do ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" da ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" dos ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" das ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" no ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" na ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" nos ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" nas ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" em ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" para ", "-", $novo_metaid);
+      $novo_metaid = str_replace(" ", "-", $novo_metaid);
+      $novo_metaid = str_replace("---", "-", $novo_metaid);
+      $novo_metaid = str_replace("--", "-", $novo_metaid);
+      $novo_metaid = str_replace(":", "", $novo_metaid);
+      $novo_metaid = str_replace(".", "", $novo_metaid);
+      $novo_metaid = str_replace(",", "", $novo_metaid);
+      $novo_metaid = str_replace("ç", "c", $novo_metaid);
+      $novo_metaid = str_replace("ã", "a", $novo_metaid);
+      $novo_metaid = str_replace("á", "a", $novo_metaid);
+      $novo_metaid = str_replace("â", "a", $novo_metaid);
+      $novo_metaid = str_replace("í", "i", $novo_metaid);
+      $novo_metaid = str_replace("ê", "e", $novo_metaid);
+      $novo_metaid = str_replace("é", "e", $novo_metaid);
+      $novo_metaid = str_replace("à", "a", $novo_metaid);
+      $novo_metaid = str_replace("ú", "u", $novo_metaid);
+      $novo_metaid = str_replace("(", "", $novo_metaid);
+      $novo_metaid = str_replace(")", "", $novo_metaid);
+      $novo_metaid = str_replace("ó", "o", $novo_metaid);
+      $novo_metaid = str_replace("ô", "o", $novo_metaid);
+      $novo_metaid = utf8_decode($novo_metaid);
+      $novo_metaid = substr($novo_metaid, 0, 30);
+      $novo_metaid = utf8_encode($novo_metaid);
+      $update = $conn->query("UPDATE Temas SET metaid = '$novo_metaid' WHERE id = '$id'");
     }
   }
-  // aqui entrará a parte de busca por temas.
-  if ($found == false) {
-    // não havendo encontrado um match exato, o sistema busca por partial matches
-    $index = 500;
-    $winner = 0;
-    $result = $conn->query("SELECT sigla, materia FROM Materias WHERE concurso = '$concurso' AND estado = 1 ORDER BY ordem");
-    if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
-        $sigla = $row["sigla"];
-        $materia = $row["materia"];
-        $materialow = mb_strtolower($materia);
-        $commandlow = mb_strtolower($command);
-        $check = levenshtein($materialow, $commandlow, 1, 1, 1);
-  			if (strpos($materialow, $commandlow) !== false) {
-  				echo "foundfoundfoundf$materia";
-          $conn->close();
-  				return;
-  			}
-        elseif ($check < $index) {
-          $index = $check;
-          $winner = $materia;
-        }
-      }
-      $length = strlen($command);
-      if ($index < $length) {
-        echo "foundfoundfoundf$winner";
-        $conn->close();
-        return;
-      }
-    }
-  }
-  echo "notfoundnotfoundnada foi encontrado";
-  $conn->close();
-  return;
 }
 
-function readSearchOptions($concurso) {
+if (isset($_POST['novo_metatema_id'])) {
+  $novo_metatema = $_POST['novo_metatema'];
+  $novo_metatema_id = $_POST['novo_metatema_id'];
+  $remover_ciclo = $_POST['remover_ciclo'];
   $servername = "localhost";
   $username = "grupoubique";
   $password = "ubique patriae memor";
   $dbname = "Ubique";
+  $found = false;
   $conn = new mysqli($servername, $username, $password, $dbname);
   mysqli_set_charset($conn,"utf8");
-  $result = $conn->query("SELECT materia, ordem FROM Materias WHERE concurso = '$concurso' AND estado = 1 ORDER BY ordem");
-  if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      $materia = $row["materia"];
-      echo "<option>$materia</option>";
-    }
+  $result = $conn->query("UPDATE Temas SET metaid = '$novo_metatema' WHERE id = '$novo_metatema_id'");
+  if ($remover_ciclo = true) {
+    $result = $conn->query("UPDATE Temas SET ciclo_revisao = 1 WHERE id = '$novo_metatema_id'");
   }
-
-  $result = $conn->query("SELECT nivel1, nivel2, nivel3 FROM Temas WHERE concurso = '$concurso' ORDER BY id, sigla_materia, nivel1, nivel2, nivel3");
-  if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      $nivel1 = $row["nivel1"];
-      $nivel2 = $row["nivel2"];
-      $nivel3 = $row["nivel3"];
-      $nivel1 = limpar_tema($nivel1);
-      $nivel2 = limpar_tema($nivel2);
-      $nivel3 = limpar_tema($nivel3);
-
-      if ($nivel3 != false) {
-        echo "<option>$nivel3</option>";
-      }
-      else {
-        if ($nivel2 != false) {
-          echo "<option>$nivel2</option>";
-        }
-        else {
-          echo "<option>$nivel</option>";
-        }
-      }
-    }
-  }
-  $conn->close();
 }
 
-?>
+<form class='text-center border border-light px-2 my-2' method='post'>
+  <p class='h4 my-4'>Acrescentar meta-tema</p>
+  <p class='text-left'>Para que o sistema seja imune a mudanças de edital, é necessário que cada tema seja internamente identificado por um meta-tema.</p>
+  <fieldset class='form-group text-left'>
+    <label for='registrarmeta'>Novo meta-tema para este assunto:</label>
+    <input name='novo_metatema' id='registrarmeta' type='text' value='$metaid'></input>
+  </fieldset>
+  <div class='custom-control custom-checkbox'>
+      <input type='checkbox' class='custom-control-input' id='remover_ciclo' name='remover_ciclo' checked>
+      <label class='custom-control-label' for='remover_ciclo'>Remover do ciclo de revisão</label>
+  </div>
+  <button name='novo_metatema_id' type='submit' class='btn btn-primary' value='$id'>Registrar novo meta-tema</button>
+</form>
 
-<script>
-
-$('#searchBarGo').click(function() {
-  var command = $('#searchBar').val();
-  var command = btoa(command);
-  var concurso = $('#searchBarGo').val();
-  var concurso = btoa(concurso);
-  $.post('engine.php', {'sbcommand': command, 'sbconcurso': concurso}, function(data) {
-    $("#searchBar").val('');
-    if (data != 0) {
-      var pw = data.substring(0, 16);
-      var pw2 = data.substring(16);
-      if (pw == 'notfoundnotfound') {
-        $("#searchBar").val(pw2);
-      }
-      else if (pw = 'foundfoundfoundf') {
-        $("#searchBar").val(pw2);
-      }
-    }
-  });
-  return false;
-});
-
-</script>
+<form class='text-center border border-light px-2 my-2' method='post'>
+  <p class='h4 my-4'>Metatemas automáticos</p>
+  <p class='text-left'>Ao pressionar o botão abaixo, todos os metatemas não-registrados serão automaticamente criados, baseados no título do tema.</p>
+  <button name='metatemas_automaticos' type='submit' class='btn btn-primary' value='$concurso'>criar metatemas automaticos</button>
+</form>
--- engine.php
+++ engine.php
@@ -200,7 +200,17 @@ function carregar_edicao_verbete($id_tema, $concurso) {
       $tema = $row['chave'];
     }
   }
+  $salvar = array($concurso, $tema);
+  $salvar = serialize($salvar);
   echo "<h1 class='mb-5'>$tema</h1>";
+  echo "
+  <form class='text-center border border-light px-2 my-2' method='post'>
+    <p class='h4 my-4'>Edição de verbete</p>
+    <p class='text-left'>Recomenda-se escrever em um meio termo entre a linguagem da Wikipédia e a da etapa discursiva do concurso.</p>
+    <textarea class='textarea_verbete' name='verbete_texto'></textarea>
+    <button name='metatemas_automaticos' type='submit' class='btn btn-primary' value='$salvar'>Salvar</button>
+  </form>
+  ";
 }
 
 function carregar_verbete($id_tema, $concurso){
@@ -533,7 +543,7 @@ function carregar_edicao_temas($concurso) {
   $found = false;
   $conn = new mysqli($servername, $username, $password, $dbname);
   mysqli_set_charset($conn,"utf8");
-  $result = $conn->query("SELECT id, sigla_materia, metaid, nivel1, nivel2, nivel3, nivel4, nivel5 FROM Temas WHERE concurso = '$concurso' AND ciclo_revisao = 0 ORDER BY id");
+  $result = $conn->query("SELECT id, sigla_materia, nivel1, nivel2, nivel3, nivel4, nivel5 FROM Temas WHERE concurso = '$concurso' AND ciclo_revisao = 0 ORDER BY id");
   if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
       $active1 = false;
@@ -543,7 +553,6 @@ function carregar_edicao_temas($concurso) {
       $active5 = false;
       $id = $row['id'];
       $sigla_materia = $row['sigla_materia'];
-      $metaid = $row['metaid'];
       $nivel1 = $row['nivel1'];
       $nivel2 = $row['nivel2'];
       $nivel3 = $row['nivel3'];
@@ -563,52 +572,17 @@ function carregar_edicao_temas($concurso) {
           <li class='list-group-item $active4'><strong>Nível 2: </strong>$nivel4</li>
           <li class='list-group-item $active5'><strong>Nível 3: </strong>$nivel5</li>
           <li class='list-group-item'><strong>ID: </strong>$id</li>
-          <li class='list-group-item'><strong>Metatema: </strong>$metaid</li>
         </ul>";
       echo "
-        <form class='text-center border border-light px-2 my-2' method='post'>
-          <p class='h4 my-4'>Acrescentar meta-tema</p>
-          <p class='text-left'>Para que o sistema seja imune a mudanças de edital, é necessário que cada tema seja internamente identificado por um meta-tema.</p>
-          <fieldset class='form-group text-left'>
-            <label for='registrarmeta'>Novo meta-tema para este assunto:</label>
-            <input name='novo_metatema' id='registrarmeta' type='text' value='$metaid'></input>
-          </fieldset>
-          <div class='custom-control custom-checkbox'>
-              <input type='checkbox' class='custom-control-input' id='remover_ciclo' name='remover_ciclo' checked>
-              <label class='custom-control-label' for='remover_ciclo'>Remover do ciclo de revisão</label>
-          </div>
-          <button name='novo_metatema_id' type='submit' class='btn btn-primary' value='$id'>Registrar novo meta-tema</button>
-        </form>
         <form class='text-center border border-light px-2 my-2' method='post'>
           <p class='h4 my-4'>Re-iniciar ciclo de revisão</p>
           <p class='text-left'>Ao pressionar o botão abaixo, todas as questões deste concurso serão colocadas no ciclo de revisão.</p>
           <button name='reiniciar_ciclo' type='submit' class='btn btn-primary' value='$concurso'>Reiniciar ciclo de revisão</button>
         </form>
-        <form class='text-center border border-light px-2 my-2' method='post'>
-          <p class='h4 my-4'>Metatemas automáticos</p>
-          <p class='text-left'>Ao pressionar o botão abaixo, todos os metatemas não-registrados serão automaticamente criados, baseados no título do tema.</p>
-          <button name='metatemas_automaticos' type='submit' class='btn btn-primary' value='$concurso'>Reiniciar ciclo de revisão</button>
-        </form>
+
       ";
       return;
     }
-  }
-}
-
-if (isset($_POST['novo_metatema_id'])) {
-  $novo_metatema = $_POST['novo_metatema'];
-  $novo_metatema_id = $_POST['novo_metatema_id'];
-  $remover_ciclo = $_POST['remover_ciclo'];
-  $servername = "localhost";
-  $username = "grupoubique";
-  $password = "ubique patriae memor";
-  $dbname = "Ubique";
-  $found = false;
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  mysqli_set_charset($conn,"utf8");
-  $result = $conn->query("UPDATE Temas SET metaid = '$novo_metatema' WHERE id = '$novo_metatema_id'");
-  if ($remover_ciclo = true) {
-    $result = $conn->query("UPDATE Temas SET ciclo_revisao = 1 WHERE id = '$novo_metatema_id'");
   }
 }
 
@@ -622,73 +596,6 @@ if (isset($_POST['reiniciar_ciclo'])) {
   $conn = new mysqli($servername, $username, $password, $dbname);
   mysqli_set_charset($conn,"utf8");
   $result = $conn->query("UPDATE Temas SET ciclo_revisao = 0 WHERE concurso = '$concurso'");
-}
-
-if (isset($_POST['metatemas_automaticos'])) {
-  $concurso = $_POST['metatemas_automaticos'];
-  $servername = "localhost";
-  $username = "grupoubique";
-  $password = "ubique patriae memor";
-  $dbname = "Ubique";
-  $found = false;
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  mysqli_set_charset($conn,"utf8");
-  $result = $conn->query("SELECT id, nivel1, nivel2, nivel3, nivel4, nivel5 FROM Temas WHERE concurso = '$concurso' AND metaid IS NULL");
-  if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      $id = $row['id'];
-      $nivel1 = $row['nivel1'];
-      $nivel2 = $row['nivel2'];
-      $nivel3 = $row['nivel3'];
-      $nivel4 = $row['nivel4'];
-      $nivel5 = $row['nivel5'];
-      if ($nivel5 != false) { $novo_metaid = $nivel5; }
-      elseif ($nivel4 != false) { $novo_metaid = $nivel4; }
-      elseif ($nivel3 != false) { $novo_metaid = $nivel3; }
-      elseif ($nivel2 != false) { $novo_metaid = $nivel2; }
-      else { $novo_metaid = $nivel1; }
-      $novo_metaid = strtolower($novo_metaid);
-      $novo_metaid = str_replace(" e ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" a ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" o ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" as ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" os ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" de ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" do ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" da ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" dos ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" das ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" no ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" na ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" nos ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" nas ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" em ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" para ", "-", $novo_metaid);
-      $novo_metaid = str_replace(" ", "-", $novo_metaid);
-      $novo_metaid = str_replace("---", "-", $novo_metaid);
-      $novo_metaid = str_replace("--", "-", $novo_metaid);
-      $novo_metaid = str_replace(":", "", $novo_metaid);
-      $novo_metaid = str_replace(".", "", $novo_metaid);
-      $novo_metaid = str_replace(",", "", $novo_metaid);
-      $novo_metaid = str_replace("ç", "c", $novo_metaid);
-      $novo_metaid = str_replace("ã", "a", $novo_metaid);
-      $novo_metaid = str_replace("á", "a", $novo_metaid);
-      $novo_metaid = str_replace("â", "a", $novo_metaid);
-      $novo_metaid = str_replace("í", "i", $novo_metaid);
-      $novo_metaid = str_replace("ê", "e", $novo_metaid);
-      $novo_metaid = str_replace("é", "e", $novo_metaid);
-      $novo_metaid = str_replace("à", "a", $novo_metaid);
-      $novo_metaid = str_replace("ú", "u", $novo_metaid);
-      $novo_metaid = str_replace("(", "", $novo_metaid);
-      $novo_metaid = str_replace(")", "", $novo_metaid);
-      $novo_metaid = str_replace("ó", "o", $novo_metaid);
-      $novo_metaid = str_replace("ô", "o", $novo_metaid);
-      $novo_metaid = utf8_decode($novo_metaid);
-      $novo_metaid = substr($novo_metaid, 0, 30);
-      $novo_metaid = utf8_encode($novo_metaid);
-      $update = $conn->query("UPDATE Temas SET metaid = '$novo_metaid' WHERE id = '$id'");
-    }
-  }
 }
 
 
