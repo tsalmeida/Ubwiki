6db9172d797ebdd3acc5f0100729fb0c7b59e643
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-17 00:21:36 +0100

    Update editar_verbete.php

--- editar_verbete.php
+++ editar_verbete.php
@@ -21,11 +21,11 @@
     $found = false;
     mysqli_set_charset($conn,"utf8");
     $result = $conn->query("SELECT verbete FROM Verbetes WHERE concurso = '$concurso' AND id_tema = '$id_tema'");
-    if ($result->num_rows = 0) {
-        $result = $conn->query("INSERT INTO Verbetes (id_tema, concurso, verbete) VALUES ('$id_tema', '$concurso', '$novo_verbete')");
+    if ($result->num_rows > 0) {
+      $result = $conn->query("UPDATE Verbetes (verbete) VALUES ('$novo_verbete')");
     }
     else {
-      $result = $conn->query("UPDATE Verbetes (verbete) VALUES ('$novo_verbete')");
+      $result = $conn->query("INSERT INTO Verbetes (id_tema, concurso, verbete) VALUES ('$id_tema', '$concurso', '$novo_verbete')");
     }
   }
 
