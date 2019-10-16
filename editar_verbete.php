c2015da430d6b1f6f8b0e8673d99c7d1352261e1
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-16 20:15:07 +0100

    verbete edit

--- editar_verbete.php
+++ editar_verbete.php
@@ -33,7 +33,7 @@
     standard_jumbotron($tema, "verbete.php?concurso=$concurso&tema=$id_tema");
     sub_jumbotron("Edição de verbete", false);
 ?>
-    <div class="container my-5">
+    <div class="container-fluid my-5">
       <div class="row">
         <div class="col-2"></div>
         <div class="col-8">
