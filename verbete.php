57e79d53c6a5af0e5b12d5aca69238207c577e72
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-16 17:26:20 +0100

    Update verbete.php

--- verbete.php
+++ verbete.php
@@ -86,7 +86,7 @@ if (isset($_GET['concurso'])) {
             $line = strtok($verbete, $separator);
 
             while ($line !== false) {
-                echo "<p class='pv'>$line</p>";
+                echo "<p>$line</p>";
                 $line = strtok( $separator );
             }
           }
