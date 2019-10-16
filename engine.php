aade3c9beeb5c75ce6929ebc8542748ac0f6ba10
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-16 22:11:14 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -86,9 +86,16 @@ function bottom_page() {
         ';
         echo "
           <script>
-            var quill = new Quill('#editor', {
-              theme: 'snow'
-            });
+          var options = {
+            debug: 'info',
+            modules: {
+              toolbar: '#toolbar'
+            },
+            placeholder: 'Compose an epic...',
+            readOnly: true,
+            theme: 'snow'
+          };
+          var editor = new Quill('#editor', options);
           </script>
         ";
       }
