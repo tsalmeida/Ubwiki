98dd0870c7417f6cd2b95017b2c1ed79a32eb14d
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-16 23:55:04 +0100

    Update editar_verbete.php

--- editar_verbete.php
+++ editar_verbete.php
@@ -65,8 +65,7 @@
             <form method='post'>
               <div class='row justify-content-center'>
                 <div id='quill_container' class='container col-12'>
-                  <textarea id='quill_editor' name='verbete_texto' class='rounded textarea_verbete px-4 py-4 my-2'>$verbete_consolidado</div>
-                </textarea>
+                  <textarea id='quill_editor' name='verbete_texto' class='rounded textarea_verbete px-4 py-4 my-2'>$verbete_consolidado</textarea>
               </div>
               <div class='row justify-content-center'>
                 <button name='salvar_verbete_texto' type='submit' class='btn btn-primary' value='$salvar'>Salvar</button>
