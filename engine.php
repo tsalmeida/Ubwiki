85630adea4d71eba7b9071893dc35f635d1a1c11
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-16 19:00:22 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -40,14 +40,19 @@ function top_page() {
       $array = 0;
       while (isset($args[$array])) {
         if ($args[$array] == "quill") {
-          echo '
+          echo "
             <!-- Main Quill library -->
-            <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
-            <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
+            <script src='//cdn.quilljs.com/1.3.6/quill.js'></script>
+            <script src='//cdn.quilljs.com/1.3.6/quill.min.js'></script>
 
             <!-- Theme included stylesheets -->
-            <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-          ';
+            <link href='//cdn.quilljs.com/1.3.6/quill.snow.css' rel='stylesheet'>
+            <script>
+              var quill = new Quill('#editor', {
+                theme: 'snow'
+              });
+            </script>
+          ";
         }
         elseif ($args[$array] == "onepage") {
           echo "
@@ -86,11 +91,6 @@ function bottom_page() {
     while (isset($args[$array])) {
       if ($args[$array] == "quill") {
         echo "
-          <script>
-            var quill = new Quill('#editor', {
-              theme: 'snow'
-            });
-          </script>
         ";
       }
       $array++;
