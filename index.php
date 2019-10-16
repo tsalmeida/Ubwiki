a935b3afd39cfc7240735100b4de1b4f3e2ea93f
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-16 07:32:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $concurso = "CACD";
 ?>
   <body>
     <?php carregar_navbar(); ?>
-    <div class="container-fluid text-center bg-white justify-content-center home1">
+    <div class="container text-center bg-white justify-content-center home1">
       <div class="row justify-content-center">
           <div class="col-lg-2">
               <img class="img-fluid logo" src="imagens/ubiquelogo.png"></img>
