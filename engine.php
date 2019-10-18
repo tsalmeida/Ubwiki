63412b253569cf3b0423e0a8f878b29438aaab5b
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-18 10:30:07 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -14,8 +14,8 @@ function carregar_navbar() {
     <div class='mr-auto'>
       <nav>
         <ol class='breadcrumb d-inline-flex pl-0 pt-0 text-dark'>
-          <li class='breadcrumb-item'><a href=''>Teste 1</a></li>
-          <li class='breadcrumb-item active h4' active>Teste 2</li>
+          <li class='breadcrumb-item h4'><a href='' class='text-dark'>Teste 1</a></li>
+          <li class='breadcrumb-item h4 text-dark'>Teste 2</li>
         </ol>
       </nav>
     </div>
