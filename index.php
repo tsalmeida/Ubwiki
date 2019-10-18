564c4e4585ae0fb7ce58d94ea6cd623633e5a5c6
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-18 23:42:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,10 @@
   }
 
   $result = $conn->query("SELECT id FROM Usuarios WHERE email = '$user'");
-  $usuario_id = $row['id'];
+  if ($result->num_rows > 0) {
+    $usuario_id = $row['id'];
+    error_log($usuario_id);
+  }
 
   if ($newuser == true) {
     $create = $conn2->query("CREATE TABLE `Ubwiki_usuarios`.`$usuario_id` ( `id` INT NOT NULL AUTO_INCREMENT , `tipo` VARCHAR(255) NOT NULL , `tipo_conteudo` VARCHAR(255) NOT NULL , `conteudo_varchar` VARCHAR(255) NOT NULL , `conteudo_text` TEXT NOT NULL , `conteudo_timestamp` TIMESTAMP NOT NULL , `conteudo_boolean` BOOLEAN NOT NULL , PRIMARY KEY (`id`)) ENGINE = MyISAM COMMENT = 'tabela de $user';");
