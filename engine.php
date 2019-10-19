215a7d69f0efefcfc2544b62b3264e27706779b7
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-19 13:10:44 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -154,10 +154,10 @@ echo "
   <div class='container-fluid py-5 col-lg-12 bg-lighter text-center mb-3'>
 ";
     if ($link == false) {
-      echo "<span class='h1'>$titulo</span>";
+      echo "<h1>$titulo</h1>";
     }
     else {
-      echo "<a href='$link' class='h1'>$titulo</a>";
+      echo "<a href='$link><h1>$titulo</h1></a>";
     }
 echo "
   </div>
