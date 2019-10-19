bd2752d381fdffdaf5b78eab6f1a5634ab098127
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-19 18:44:19 +0100

    Update engine_criar_subtopicos.php

--- engine_criar_subtopicos.php
+++ engine_criar_subtopicos.php
@@ -247,7 +247,7 @@ if ($refazer_ordem == true) {
   $servername = "localhost"; $username = "grupoubique"; $password = "ubique patriae memor"; $dbname = "Ubique";
   $conn = new mysqli($servername, $username, $password, $dbname); mysqli_set_charset($conn,"utf8");
   $ordem = 0;
-  $result = $conn->query("SELECT id FROM Temas WHERE concurso = '$concurso' ORDER BY nivel5, nivel4, nivel4, nivel2, nivel1, sigla_materia, id");
+  $result = $conn->query("SELECT id FROM Temas WHERE concurso = '$concurso' AND sigla_materia = '$sigla_materia' ORDER BY id, sigla_materia, nivel1, nivel2, nivel3, nivel4, nivel5");
   if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
       $ordem++;
