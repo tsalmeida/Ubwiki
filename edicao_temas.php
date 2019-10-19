b7619a8cf9852f00023161b93793c1f8325667be
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-19 08:45:55 +0100

    Update edicao_temas.php

--- edicao_temas.php
+++ edicao_temas.php
@@ -31,8 +31,8 @@
     <?php
     standard_jumbotron("Edição de temas", false);
     ?>
-    <div class="container my-5">
-      <div class="row">
+    <div class="container-fluid my-5">
+      <div class="row justify-content-center">
         <div class="col-8">
           <?php
             echo "<h1>$concurso</h1>";
