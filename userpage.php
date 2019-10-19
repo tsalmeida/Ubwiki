d94f3fdd1459d0940134e589cf7a8638e64f4f4d
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-19 12:34:49 +0100

    Update userpage.php

--- userpage.php
+++ userpage.php
@@ -10,19 +10,47 @@
 
   include 'engine.php';
   top_page();
+
+  $result = $conn->query("SELECT id, tipo, criacao, apelido, senha, nome, sobrenome, concursos FROM Usuarios WHERE email = '$user'");
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      $id = $row['id'];
+      $tipo = $row['tipo'];
+      $apelido = $row['apelido'];
+      $nome = $row['nome'];
+      $sobrenome = $row['sobrenome'];
+      $concursos = $row['concursos'];
+    }
+  }
+
 ?>
   <body>
     <?php
       carregar_navbar();
       standard_jumbotron("Sua página", false);
     ?>
-    <div class="container my-5">
+    <div class="container-fluid my-5">
       <div class="row">
-        <div class="col-2"></div>
         <div class="col-8">
-          <p>Informações do usuário aqui. Verbetes em que colaborou, comentários que escreveu, temas em que já fez anotações, temas prioritários etc.</p>
+
+          <?php
+            if ($tipo = 'admin') {
+              echo "
+                <h2>Administrador</h2>
+                <p>Você tem acesso à <a href='admin.php'>página de administrador</a>.</p>
+              ";
+            }
+            echo "
+              <h2>Dados da sua conta:</h2>
+              <ul class='list-group'>
+                <li class='list-group-item'>Apelido: $apelido</li>
+                <li class='list-group-item'>Nome: $nome</li>
+                <li class='list-group-item'>Sobrenome: $sobrenome</li>
+              </ul>
+            ";
+          ?>
+
         </div>
-        <div class="col-2"></div>
       </div>
     </div>
   </body>
