f915a965a73984c39780c9126d6f36bb9987c7ce
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-20 13:27:11 +0100

    Update engine_criar_subtopicos.php

--- engine_criar_subtopicos.php
+++ engine_criar_subtopicos.php
@@ -4,7 +4,6 @@ $refazer_ordem = false;
 
 if (isset($_POST['form_sigla_materia'])) {
   $form_nivel1 = $_POST['form_nivel1']; $form_nivel2 = $_POST['form_nivel2']; $form_nivel3 = $_POST['form_nivel3']; $form_nivel4 = $_POST['form_nivel4']; $form_nivel5 = $_POST['form_nivel5']; $form_sigla_materia = $_POST['form_sigla_materia']; $form_nivel = $_POST['form_nivel'];
-
 }
 
 if ((isset($_POST['topico_subalterno1'])) && ($_POST['topico_subalterno1'] != "")) {
@@ -34,7 +33,6 @@ if ((isset($_POST['topico_subalterno2'])) && ($_POST['topico_subalterno2'] != ""
 if ((isset($_POST['topico_subalterno3'])) && ($_POST['topico_subalterno3'] != "")) {
   $refazer_ordem = true;
   $novo_subtopico = $_POST['topico_subalterno3'];
-
   $servername = "localhost"; $username = "grupoubique"; $password = "ubique patriae memor"; $dbname = "Ubique";
   $conn = new mysqli($servername, $username, $password, $dbname); mysqli_set_charset($conn,"utf8");
   if ($form_nivel == 1) { $insert = $conn->query("INSERT INTO Temas (ciclo_revisao, concurso, sigla_materia, nivel, nivel1, nivel2) VALUES (0, '$concurso', '$form_sigla_materia', 2, '$form_nivel1', '$novo_subtopico') "); }
@@ -47,7 +45,6 @@ if ((isset($_POST['topico_subalterno3'])) && ($_POST['topico_subalterno3'] != ""
 if ((isset($_POST['topico_subalterno4'])) && ($_POST['topico_subalterno4'] != "")) {
   $refazer_ordem = true;
   $novo_subtopico = $_POST['topico_subalterno4'];
-
   $servername = "localhost"; $username = "grupoubique"; $password = "ubique patriae memor"; $dbname = "Ubique";
   $conn = new mysqli($servername, $username, $password, $dbname); mysqli_set_charset($conn,"utf8");
   if ($form_nivel == 1) { $insert = $conn->query("INSERT INTO Temas (ciclo_revisao, concurso, sigla_materia, nivel, nivel1, nivel2) VALUES (0, '$concurso', '$form_sigla_materia', 2, '$form_nivel1', '$novo_subtopico') "); }
@@ -60,7 +57,6 @@ if ((isset($_POST['topico_subalterno4'])) && ($_POST['topico_subalterno4'] != ""
 if((isset($_POST['topico_subalterno5'])) && ($_POST['topico_subalterno5'] != "")) {
   $refazer_ordem = true;
   $novo_subtopico = $_POST['topico_subalterno5'];
-
   $servername = "localhost"; $username = "grupoubique"; $password = "ubique patriae memor"; $dbname = "Ubique";
   $conn = new mysqli($servername, $username, $password, $dbname); mysqli_set_charset($conn,"utf8");
   if ($form_nivel == 1) { $insert = $conn->query("INSERT INTO Temas (ciclo_revisao, concurso, sigla_materia, nivel, nivel1, nivel2) VALUES (0, '$concurso', '$form_sigla_materia', 2, '$form_nivel1', '$novo_subtopico') "); }
@@ -73,7 +69,6 @@ if((isset($_POST['topico_subalterno5'])) && ($_POST['topico_subalterno5'] != "")
 if((isset($_POST['topico_subalterno6'])) && ($_POST['topico_subalterno6'] != "")) {
   $refazer_ordem = true;
   $novo_subtopico = $_POST['topico_subalterno6'];
-
   $servername = "localhost"; $username = "grupoubique"; $password = "ubique patriae memor"; $dbname = "Ubique";
   $conn = new mysqli($servername, $username, $password, $dbname); mysqli_set_charset($conn,"utf8");
   if ($form_nivel == 1) { $insert = $conn->query("INSERT INTO Temas (ciclo_revisao, concurso, sigla_materia, nivel, nivel1, nivel2) VALUES (0, '$concurso', '$form_sigla_materia', 2, '$form_nivel1', '$novo_subtopico') "); }
@@ -86,7 +81,6 @@ if((isset($_POST['topico_subalterno6'])) && ($_POST['topico_subalterno6'] != "")
 if((isset($_POST['topico_subalterno7'])) && ($_POST['topico_subalterno7'] != "")) {
   $refazer_ordem = true;
   $novo_subtopico = $_POST['topico_subalterno7'];
-
   $servername = "localhost"; $username = "grupoubique"; $password = "ubique patriae memor"; $dbname = "Ubique";
   $conn = new mysqli($servername, $username, $password, $dbname); mysqli_set_charset($conn,"utf8");
   if ($form_nivel == 1) { $insert = $conn->query("INSERT INTO Temas (ciclo_revisao, concurso, sigla_materia, nivel, nivel1, nivel2) VALUES (0, '$concurso', '$form_sigla_materia', 2, '$form_nivel1', '$novo_subtopico') "); }
