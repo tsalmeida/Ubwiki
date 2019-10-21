5796e8b0b0c4b0bb9f15c879a55102250936010b
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-21 21:35:31 +0100

    Update quill.php

--- quill.php
+++ quill.php
@@ -38,19 +38,35 @@
 
   var container = quill.addContainer('ql-around');
   var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
 
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+
+    ['clean']                                         // remove formatting button
   ];
 
-  var options = {
+  var quill = new Quill('#editor', {
     debug: 'info',
     modules: {
       toolbar: {
-        container: '#toolbar',
+        toolbarOptions,
+        container: '#toolbar'
       }
-      toolbar: toolbarOptions
     },
     theme: 'snow'
-  }
+  });
 
   var editor = new Quill('#editor', options);
 
