bb8ba3a7e02afde476ebb145fdf6ec792d649239
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-21 12:49:03 +0100

    Update userpage.php

--- userpage.php
+++ userpage.php
@@ -44,13 +44,37 @@
               <h2 class='text-center'>Dados da sua conta:</h2>
               <ul class='list-group'>
                 <li class='list-group-item'><strong>Conta criada em:</strong> $criacao</li>
-                <li class='list-group-item'><a href='#'><i class='fal fa-edit'></i></a> <strong>Apelido:</strong> $apelido</li>
+                <li class='list-group-item'><a data-toggle='modal' data-target='#modal_editar_apelido' href=''><i class='fal fa-edit'></i></a> <strong>Apelido:</strong> $apelido</li>
                 <li class='list-group-item'><a href='#'><i class='fal fa-edit'></i></a> <strong>Nome:</strong> $nome</li>
                 <li class='list-group-item'><a href='#'><i class='fal fa-edit'></i></a> <strong>Sobrenome:</strong> $sobrenome</li>
                 <li class='list-group-item'><strong>Email:</strong> $user</li>
               </ul>
             ";
           ?>
+        </div>
+      </div>
+    </div>
+    <div id='modal_editar_apelido' class='modal-fade' role='dialog' tabindex='-1'>
+      <div class='modal-dialog modal-dialog-centered' role='document'>
+        <div class='modal-content'>
+          <form method='post'>
+            <div class='modal-header text-center'>
+              <h4 class='modal-title w-100 font-weight-bold'>Determinar apelido</h4>
+              <button type='button' class='close' data-dismiss='modal'>
+                <i class="fal fa-times-circle"></i>
+              </button>
+            </div>
+            <div class='modal-body mx-3'>
+              <div class='md-form md-2'>
+                <input type='text' name='novo_apelido' class='form-control validate'></input>
+                <label data-error='preenchimento incorreto' data-successd='preenchimento correto' for='novo_link_link' required>Novo apelido</label>
+              </div>
+            </div>
+            <div class='modal-footer d-flex justify-content-center'>
+              <button type='button' class='btn bg-lighter btn-lg' data-dismiss='modal'><i class="fal fa-times-circle"></i> Cancelar</button>
+              <button type='submit' class='but btn-primary btn-lg'><i class='fal fa-check'></i> Salvar</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
