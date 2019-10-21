ec73cfdd4d7b331ac064aee4d97a96b29afa02c4
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-21 11:04:46 +0100

    col size

--- logout.php
+++ logout.php
@@ -19,7 +19,7 @@
     ?>
     <div class="container-fluid my-5">
       <div class="row justify-content-center">
-        <div class="col-8">
+        <div class="col-6">
           <h1>Logout realizado</h1>
           <p>Para retornar, por favor siga o link na <a href='https://www.grupoubique.com.br'>página do Grupo Ubique</a>.</p>
         </div>
--- termos.php
+++ termos.php
@@ -17,16 +17,14 @@
     carregar_navbar();
     standard_jumbotron("Ubwiki", false);
     ?>
-    <div class="container my-5">
-      <div class="row">
-        <div class="col-2"></div>
-        <div class="col-8">
+    <div class="container-fluid my-5">
+      <div class="row justify-content-center">
+        <div class="col-6">
           <?php
           $result = extract_gdoc("https://docs.google.com/document/d/e/2PACX-1vTAJII7h1Fm2ndrB-KjqH2w4CvwfyKKcr5myjh_IfqCIe7-Ai9JZWj6wlNt5shG_wbNv0_KVELPGU6W/pub?embedded=true");
           echo $result;
           ?>
         </div>
-        <div class="col-2"></div>
       </div>
     </div>
   </body>
