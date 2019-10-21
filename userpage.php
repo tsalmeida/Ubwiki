d7f9523ae0a5a0f11cbd71c5cfc0001c5039e808
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-21 17:01:04 +0100

    Update userpage.php

--- userpage.php
+++ userpage.php
@@ -42,7 +42,7 @@
           <?php
             echo "
               <div class='row justify-content-center'>
-                <h2 class='text-center'>Dados da sua conta:</h2><a data-toggle='modal' data-target='#modal_editar_dados' href=''><i class='fal fa-edit'></i></a>
+                <h3 class='text-center'><a data-toggle='modal' data-target='#modal_editar_dados' href=''><i class='fal fa-edit'></i></a>Dados da sua conta:</h3>
               </div>
               <ul class='list-group'>
                 <li class='list-group-item'><strong>Conta criada em:</strong> $criacao</li>
@@ -61,16 +61,29 @@
         <div class='modal-content'>
           <form method='post'>
             <div class='modal-header text-center'>
-              <h4 class='modal-title w-100 font-weight-bold'>Alterar apelido</h4>
+              <h4 class='modal-title w-100 font-weight-bold'>Alterar dados</h4>
               <button type='button' class='close' data-dismiss='modal'>
                 <i class="fal fa-times-circle"></i>
               </button>
             </div>
             <div class='modal-body mx-3'>
-              <p>Seu apelido é sua identificação nos fórums de debate na página de cada verbete.</p>
+              <p>Você é identificado por seu apelido em todas as circunstâncias da página em que sua participação ou contribuição sejam tornadas públicas.</p>
               <div class='md-form md-2'>
-                <input type='text' name='novo_apelido' class='form-control validate'></input>
-                <label data-error='preenchimento incorreto' data-successd='preenchimento correto' for='novo_link_link' required>Novo apelido</label>
+                <input type='text' name='novo_apelido' id='novo_apelido' class='form-control validate' value='$apelido' required></input>
+                <label data-error='preenchimento incorreto' data-successd='preenchimento correto' for='novo_apelido' required>Apelido</label>
+              </div>
+            </div>
+            <div class='modal-body mx-3'>
+              <p>Seu nome e seu sobrenome não serão tornados públicos em nenhuma seção pública da página.</p>
+              <div class='md-form md-2'>
+                <input type='text' name='novo_nome' id='novo_nome' class='form-control validate' value='$nome' required></input>
+                <label data-error='preenchimento incorreto' data-successd='preenchimento correto' for='novo_nome'>Nome</label>
+              </div>
+            </div>
+            <div class='modal-body mx-3'>
+              <div class='md-form md-2'>
+                <input type='text' name='novo_sobrenome' id='novo_sobrenome' class='form-control validate' value='$sobrenome' required></input>
+                <label data-error='preenchimento incorreto' data-successd='preenchimento correto' for='novo_sobrenome' required>Sobrenome</label>
               </div>
             </div>
             <div class='modal-footer d-flex justify-content-center'>
