610acaaef949dd7211fe3b8761530ba2462206ed
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-21 17:30:11 +0100

    Update userpage.php

--- userpage.php
+++ userpage.php
@@ -12,6 +12,14 @@
   include 'engine.php';
   top_page();
 
+  if (isset($_POST['novo_nome'])) {
+    $novonome = $_POST['novo_nome'];
+    $novosobrenome = $_POST['novo_sobrenome'];
+    $novoapelido = $_POST['novo_apelido'];
+    $result = $conn->query("UPDATE Usuarios SET nome = '$novonome', sobrenome = '$novosobrenome', apelido = '$novoapelido' WHERE id = $id");
+  }
+
+
   $result = $conn->query("SELECT id, tipo, criacao, apelido, nome, sobrenome FROM Usuarios WHERE email = '$user'");
   if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
@@ -23,14 +31,6 @@
       $sobrenome = $row['sobrenome'];
     }
   }
-
-  if (isset($_POST['novo_nome'])) {
-    $novonome = $_POST['novo_nome'];
-    $novosobrenome = $_POST['novo_sobrenome'];
-    $novoapelido = $_POST['novo_apelido'];
-    $result = $conn->query("UPDATE Usuarios SET nome = '$novonome', sobrenome = '$novosobrenome', apelido = '$novoapelido' WHERE id = $id");
-  }
-
 
 ?>
   <body>
