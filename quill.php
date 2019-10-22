38f8774798dac0921fed81bbcec20d43d4a33822
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-22 22:55:57 +0100

    Revert "Update quill.php"
    This reverts commit 153723c9fe74747bcac578945daa7b5aac05b6d3.

--- quill.php
+++ quill.php
@@ -1,20 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Summernote</title>
-  <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-  <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
-  <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet">
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.js"></script>
-</head>
-<body>
-  <div id="summernote"><p>Hello Summernote</p></div>
-  <script>
-    $(document).ready(function() {
-        $('#summernote').summernote();
-    });
-  </script>
-</body>
-</html>
+<!-- Include stylesheet -->
+<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+
+<!-- Create the editor container -->
+<div id="editor">
+  <p>Hello World!</p>
+  <p>Some <strong>initial</strong> text</p>
+  <p><br></p>
+</div>
+
+<!-- Include the Quill library -->
+<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+
+<!-- Initialize Quill editor -->
+<script>
+
+var toolbarOptions = [
+  ['italic'],        // toggled buttons
+  ['blockquote'],
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'super' }],
+  ['image'],
+  [{ 'header': [2, false] }],
+
+  ['clean']
+];
+
+var formatWhitelist = ['italic','script','link','blockquote','list','image','header'];
+
+  var quill = new Quill('#editor', {
+    theme: 'snow',
+    formats: formatWhitelist,
+    modules: {
+      toolbar: toolbarOptions
+    }
+  });
+</script>
