42a48ed91feee1c29b4ea9c61a81da0d7442077b
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-22 07:17:05 +0100

    Update quill.php

--- quill.php
+++ quill.php
@@ -39,7 +39,6 @@
     [{ 'header': 1 }, { 'header': 2 }],               // custom button values
     [{ 'list': 'ordered'}, { 'list': 'bullet' }],
     [{ 'script': 'super' }],                            // superscript
-    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
     [{ 'direction': 'rtl' }],                         // text direction
 
     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
