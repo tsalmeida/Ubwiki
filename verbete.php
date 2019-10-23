f08da58394668397a0f9634ca7b367ba864982ab
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-23 15:14:55 +0100

    Update verbete.php

--- verbete.php
+++ verbete.php
@@ -154,8 +154,19 @@ function ler_relacionados($id_tema, $concurso) {
 <?php
   carregar_navbar();
   standard_jumbotron($tema, false);
+  $breadcrumbs = "
+    <li class='breadcrumb-item text-muted2'><a href='index.php'>$concurso</a></li>
+    <li class='breadcrumb-item text-muted2'><a href='materia.php?concurso=$concurso&sigla=$sigla_materia'>$materia</a></li>
+    <li class='breadcrumb-item text-muted2'>$nivel1</li>
+  ";
+  if ($nivel2 != false) { $breadcrumbs .= "<li class='breadcrumb-item text-muted2'>$nivel2</li>"; }
+  if ($nivel3 != false) { $breadcrumbs .= "<li class='breadcrumb-item text-muted2'>$nivel3</li>"; }
+  if ($nivel4 != false) { $breadcrumbs .= "<li class='breadcrumb-item text-muted2'>$nivel4</li>"; }
+  if ($nivel5 != false) { $breadcrumbs .= "<li class='breadcrumb-item text-muted2'>$nivel5</li>"; }
+  breadcrumbs($breadcrumbs);
 ?>
-  <div class='container-fluid py-3 col-12 bg-lighter text-center'>
+
+  <div class='container-fluid py-3 col-2 bg-lighter text-center'>
     <ul class='list-group list-group-horizontal-lg'>
       <a class='list-group-item list-group-item-action bg-lighter text-dark border-0' href='#verbete' title='Verbete'><i class="fal fa-file-alt fa-2x fa-fw"></i> <span class='d-block'>Verbete</span></a>
       <a class='list-group-item list-group-item-action bg-lighter text-dark border-0' href='#imagens' title='Imagens'><i class="fal fa-images fa-2x fa-fw"></i> <span class='d-block'>Imagens</span></a>
@@ -168,18 +179,6 @@ function ler_relacionados($id_tema, $concurso) {
       <a class='list-group-item list-group-item-action bg-lighter text-dark border-0' href='#discussao' title='Fórum'><i class="fal fa-comments-alt fa-2x fa-fw"></i> <span class='d-block'>Fórum</span></a>
     </ul>
   </div>
-  <?php
-    $breadcrumbs = "
-      <li class='breadcrumb-item text-muted2'><a href='index.php'>$concurso</a></li>
-      <li class='breadcrumb-item text-muted2'><a href='materia.php?concurso=$concurso&sigla=$sigla_materia'>$materia</a></li>
-      <li class='breadcrumb-item text-muted2'>$nivel1</li>
-    ";
-    if ($nivel2 != false) { $breadcrumbs .= "<li class='breadcrumb-item text-muted2'>$nivel2</li>"; }
-    if ($nivel3 != false) { $breadcrumbs .= "<li class='breadcrumb-item text-muted2'>$nivel3</li>"; }
-    if ($nivel4 != false) { $breadcrumbs .= "<li class='breadcrumb-item text-muted2'>$nivel4</li>"; }
-    if ($nivel5 != false) { $breadcrumbs .= "<li class='breadcrumb-item text-muted2'>$nivel5</li>"; }
-    breadcrumbs($breadcrumbs);
-  ?>
   <div class='container my-5' id='verbete'>
     <div class='row justify-content-between h3 my-5'>
       <div class='col-lg-4 col-sm-8 text-center justify-content-center align-middle'>
