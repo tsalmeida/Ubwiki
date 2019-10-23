000844c8be7d4c55176cae9717a4cb209d10041e
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-23 08:17:07 +0100

    quill styling

--- verbete.php
+++ verbete.php
@@ -560,7 +560,7 @@ function ler_relacionados($id_tema, $concurso) {
   </div>
 
   <div class='modal fade' id='modal_editar_verbete' role='dialog' tabindex='-1'>
-    <div class='modal-dialog modal-md' role='document'>
+    <div class='modal-dialog modal-lg' role='document'>
       <div class='modal-content'>
         <form id='quill_verbete_form' method='post'>
           <input name='quill_novo_verbete_html' type='hidden'>
