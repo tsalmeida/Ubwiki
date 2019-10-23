5f6a64a86f41587846d33387cbc24eae1d962d66
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-23 20:51:57 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -98,7 +98,7 @@
     $admin_mensagens = $nova_mensagem;
   }
   else {
-    $results = $conn->query("SELECT conteudo FROM Admin_data WHERE tipo = 'notas' ORDER BY id DESC");
+    $result = $conn->query("SELECT conteudo FROM Admin_data WHERE tipo = 'notas' ORDER BY id DESC");
     if ($result->num_rows > 0) {
       while($row = $result->fetch_assoc()) {
         $admin_mensagens = $row['conteudo'];
