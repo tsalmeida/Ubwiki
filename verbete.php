ef5d9a48953323ff472d110599d40cf3772b10f8
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-23 23:41:24 +0100

    Update verbete.php

--- verbete.php
+++ verbete.php
@@ -148,7 +148,7 @@ if (isset($_POST['novo_video_titulo'])) {
 ?>
 <body>
   <?php
-    carregar_navbar('dark', 'forum');
+    carregar_navbar('dark');
     $breadcrumbs = "
       <li class='breadcrumb-item'><a href='index.php'>$concurso</a></li>
       <li class='breadcrumb-item'><a href='materia.php?concurso=$concurso&sigla=$sigla_materia'>$materia</a></li>
