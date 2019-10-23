124f92cdfc74be370be9046496a96e027f4dd57a
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-23 07:57:08 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -138,7 +138,7 @@ function bottom_page() {
 
             var form = document.querySelector('#quill_verbete_form');
             form.onsubmit = function() {
-              var test = verbete_editor.root.innerHTML();
+              var test = 'teste de caneta';
               alert(test);
               var quill_novo_verbete_html = document.querySelector('input[name=quill_novo_verbete_html]');
               quill_novo_verbete_html.value = verbete_editor.root.innerHTML();
