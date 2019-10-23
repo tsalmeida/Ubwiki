a7f2baec6dcc8971c4f053f1fe298efe9b11c4fe
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-23 07:32:16 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -138,8 +138,8 @@ function bottom_page() {
 
         var form = document.querySelector('#quill_verbete_form');
         form.onsubmit = function() {
-          alert('this happened');
           var quill_novo_verbete_html = document.querySelector('input[name=quill_novo_verbete_html]');
+          alert(quill_novo_verbete_html);
           quill_novo_verbete_html.value = JSON.stringify(quill.root.innerHTML());
           var test = quill.root.innerHTML();
           alert(test);
