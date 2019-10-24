ddc28f2af6ab11369288d37b325d408aca3a4a1c
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-24 22:26:40 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -145,7 +145,7 @@
                 <label for="editar_topicos_concurso">Concurso</label>
                 <select class="form-control" id="editar_topicos_concurso">
 <?php
-                  foreach ($listaconcursos as $um_concurso) {
+                  foreach ($lista_concursos as $um_concurso) {
                     if ($um_concurso(2) == 0) { $estado = '(desativado)'; } else { $estado = '(ativado)'; }
                     echo "<option>$um_concurso(0): $um_concurso(1) / $estado</option>";
                     $count++;
