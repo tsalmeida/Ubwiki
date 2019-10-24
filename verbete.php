62ff71a01ac3afce72e65337706d131d7ec99282
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-24 11:12:58 +0100

    Update verbete.php

--- verbete.php
+++ verbete.php
@@ -162,7 +162,7 @@ if (isset($_POST['novo_video_titulo'])) {
   ?>
 
   <div class='container-fluid text-center'>
-    <div class='row'>
+    <div class='row justify-content-between'>
       <div class='col-lg-2 col-sm-12 justify-content-center d-none d-lg-block'>
         <div class='border p-3 mt-3 left-menu-nav elegant-color text-white position-fixed'>
           <ul class='list-group text-center'>
@@ -177,12 +177,12 @@ if (isset($_POST['novo_video_titulo'])) {
           </ul>
         </div>
       </div>
-      <div class='col-lg-8 col-sm-12 bg-white'>
+      <div class='col-lg-6 col-sm-12 bg-white'>
         <div class='row my-5 text-left' id='verbete'>
           <div class='col-10 text-left'>
             <?php
               echo "<span class='display-2 wireone d-none d-lg-inline'>$tema</span>";
-              echo "<span class='display-3 wireone d-block d-lg-none'>$tema</span>";
+              echo "<span class='display-3 wireone d-inline d-lg-none'>$tema</span>";
             ?>
           </div>
           <div class='col-2 h3 text-right'>
