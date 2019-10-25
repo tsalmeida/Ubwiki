c3304135b418b025f8ef7ab104f7b7691b397257
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-25 23:16:04 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
     });
     return false;
   });
-	$(function () {
-		$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-	});
+});
+$(function () {
+	$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
 });
