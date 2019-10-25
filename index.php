b09eac161ef1f17e1f1648b4ed3f9ad22cc5c655
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-26 00:12:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
       ?>
       <div class="container-fluid bg-white">
         <div class="row justify-content-center">
-            <div class="col-lg-2 col-sm-5 px-3 mt-5">
+            <div class="col-lg-2 col-sm-12 px-3 mt-5 pt-5">
                 <img class="img-fluid logo" src="imagens/ubiquelogo.png"></img>
             </div>
         </div>
