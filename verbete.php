8104983a7e810c18e1e50837af85f199b6700455
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-25 15:07:05 +0100

    Update verbete.php

--- verbete.php
+++ verbete.php
@@ -165,10 +165,10 @@ if (isset($_POST['novo_video_titulo'])) {
       <div class='col-12 justify-contents-right'>
         <div class='btn-group btn-group-toggle'>
           <label class='btn btn-primary btn-sm active' data-toggle='collapse' data-target='#anotacoes'><input type='checkbox' autocomplete='off' checked>Anotações</label>
-          <label class='btn btn-primary btn-sm active' data-toggle='collapse' data-target='#questoes'><input type='checkbox' autocomplete='off'>Questões</label>
-          <label class='btn btn-primary btn-sm active' data-toggle='collapse' data-target='#bibliografia'><input type='checkbox' autocomplete='off'>Bibliografia</label>
-          <label class='btn btn-primary btn-sm active' data-toggle='collapse' data-target='#links'><input type='checkbox' autocomplete='off'>Links</label>
-          <label class='btn btn-primary btn-sm active' data-toggle='collapse' data-target='#discussao'><input type='checkbox' autocomplete='off'>Fórum</label>
+          <label class='btn btn-primary btn-sm' data-toggle='collapse' data-target='#questoes'><input type='checkbox' autocomplete='off'>Questões</label>
+          <label class='btn btn-primary btn-sm' data-toggle='collapse' data-target='#bibliografia'><input type='checkbox' autocomplete='off'>Bibliografia</label>
+          <label class='btn btn-primary btn-sm' data-toggle='collapse' data-target='#links'><input type='checkbox' autocomplete='off'>Links</label>
+          <label class='btn btn-primary btn-sm' data-toggle='collapse' data-target='#discussao'><input type='checkbox' autocomplete='off'>Fórum</label>
         </div>
       </div>
     </div>
