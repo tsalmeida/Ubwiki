971066b16ca27bd4b07696355b29f3c62736b8e0
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-25 23:59:07 +0100

    mdb pro

--- js/dist/tests/material-select.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var QUnit = window.QUnit;
-window.QUnit = QUnit;
-QUnit.module('Material select');
-QUnit.test('should do nothing', function (assert) {
-  assert.ok(true);
-});
--- js/src/tests/material-select.js
+++
@@ -1,9 +0,0 @@
-const QUnit = window.QUnit;
-window.QUnit = QUnit;
-
-QUnit.module('Material select');
-
-QUnit.test('should do nothing', (assert) => {
-
-  assert.ok(true);
-});
