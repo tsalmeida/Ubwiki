82fd343ade8a345f979148bb41869b0d831757af
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-26 17:38:58 +0100

    Update verbete.php

--- verbete.php
+++ verbete.php
@@ -252,10 +252,10 @@ if (isset($_POST['novo_video_titulo'])) {
                           $imagem_resolucao = $row['resolucao'];
                           error_log("$imagem_titulo $imagem_arquivo $imagem_resolucao");
                           echo "
-                            <div class='carousel-item active'>
+                            <div class='carousel-item'>
                               <div class='col-12 col-md-4'>
                                 <div class='card mb-2'>
-                                  <img class='card-img-top' src='imagens/verbetes/thumbnails/$imagem_arquivo'>
+                                  <img class='card-img-top' src='imagens/verbetes/thumbnails/$imagem_arquivo'></img>
                                   <div class='card-body'>
                                     <h4>$imagem_titulo</h4>
                                     <p class='card-text'>$imagem_comentario</p>
