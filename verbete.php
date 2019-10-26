684949f45a36cc7c21ba25d333f0dfd98768a2db
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-26 10:29:20 +0100

    Update verbete.php

--- verbete.php
+++ verbete.php
@@ -225,8 +225,63 @@ if (isset($_POST['novo_video_titulo'])) {
           </div>
           <div class='row justify-content-center border-bottom border-dark py-5'>
             <div class='col-12 text-left font-weight-normal'>
+
+              <div class="row">
+                <div class="col-md-12">
+                  <div id="mdb-lightbox-ui"></div>
+                  <div class="mdb-lightbox no-margin">
+                    <figure class="col-md-4">
+                      <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg" data-size="1600x1067">
+                        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(117).jpg" class="img-fluid">
+                      </a>
+                    </figure>
+                    <figure class="col-md-4">
+                      <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg" data-size="1600x1067">
+                        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(98).jpg" class="img-fluid" />
+                      </a>
+                    </figure>
+                    <figure class="col-md-4">
+                      <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(131).jpg" data-size="1600x1067">
+                        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(131).jpg" class="img-fluid" />
+                      </a>
+                    </figure>
+                    <figure class="col-md-4">
+                      <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg" data-size="1600x1067">
+                        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(123).jpg" class="img-fluid" />
+                      </a>
+                    </figure>
+                    <figure class="col-md-4">
+                      <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg" data-size="1600x1067">
+                        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(118).jpg" class="img-fluid" />
+                      </a>
+                    </figure>
+                    <figure class="col-md-4">
+                      <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg" data-size="1600x1067">
+                        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(128).jpg" class="img-fluid" />
+                      </a>
+                    </figure>
+                    <figure class="col-md-4">
+                      <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(132).jpg" data-size="1600x1067">
+                        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(132).jpg" class="img-fluid" />
+                      </a>
+                    </figure>
+                    <figure class="col-md-4">
+                      <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(115).jpg" data-size="1600x1067">
+                        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(115).jpg" class="img-fluid" />
+                      </a>
+                    </figure>
+                    <figure class="col-md-4">
+                      <a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg" data-size="1600x1067">
+                        <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(133).jpg" class="img-fluid" />
+                      </a>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+
               <?php
-              $result = $conn->query("SELECT titulo, link, arquivo, comentario FROM Imagens WHERE id_tema = $id_tema AND concurso = '$concurso'");
+              // $result = $conn->query("SELECT titulo, link, arquivo, comentario FROM Imagens WHERE id_tema = $id_tema AND concurso = '$concurso'");
+              $result = false;
               if ($result->num_rows > 0) {
                 echo "
                 <div class='row'>
