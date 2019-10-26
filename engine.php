795471311b3ae10f380b66f22111bcdf7a7bd851
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-26 17:47:22 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -283,21 +283,23 @@ function bottom_page() {
       }
       elseif ($args[$array] == 'carousel') {
         echo "
-          $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
-            var next = $(this).next();
-            if (!next.length) {
-              next = $(this).siblings(':first');
-            }
-            next.children(':first-child').clone().appendTo($(this));
-
-            for (var i=0;i<4;i++) {
-              next=next.next();
+          <script type='text/javascript'>
+            $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+              var next = $(this).next();
               if (!next.length) {
-                next=$(this).siblings(':first');
+                next = $(this).siblings(':first');
               }
               next.children(':first-child').clone().appendTo($(this));
-            }
-          });
+
+              for (var i=0;i<4;i++) {
+                next=next.next();
+                if (!next.length) {
+                  next=$(this).siblings(':first');
+                }
+                next.children(':first-child').clone().appendTo($(this));
+              }
+            });
+          </script>
         ";
       }
       $array++;
