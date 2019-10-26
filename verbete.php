029ca884a062bf0004fa7e0e53ab65f821427f68
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-26 23:55:04 +0100

    Update verbete.php

--- verbete.php
+++ verbete.php
@@ -163,9 +163,9 @@ if (isset($_POST['novo_video_titulo'])) {
     <div class='row'>
       <div class='col-12 d-flex justify-content-center'>
         <button class='btn btn-primary btn-sm verbete_collapse collapse' data-toggle='collapse' data-target='.verbete_collapse' href='#inicio'>Verbete</button>
-        <button class='btn btn-primary btn-sm imagens_collapse ml-1' data-toggle='collapse' data-target='.imagens_collapse' href='#imagens'>Imagens</button>
+        <button class='btn btn-primary btn-sm imagens_collapse collapse ml-1' data-toggle='collapse' data-target='.imagens_collapse' href='#imagens'>Imagens</button>
         <button class='btn btn-primary btn-sm videos_collapse ml-1 collapse' data-toggle='collapse' data-target='.videos_collapse' href='#videos'>Vídeos</button>
-        <button class='btn btn-primary btn-sm bibliografia_collapse ml-1' data-toggle='collapse' data-target='.bibliografia_collapse' href='#bibliografia'>Leia mais</button>
+        <button class='btn btn-primary btn-sm bibliografia_collapse collapse ml-1' data-toggle='collapse' data-target='.bibliografia_collapse' href='#bibliografia'>Leia mais</button>
         <button class='btn btn-primary btn-sm collapse anotacoes_collapse ml-1 collapse' data-toggle='collapse' data-target='.anotacoes_collapse' href='#anotacoes'>Anotações</button>
       </div>
     </div>
