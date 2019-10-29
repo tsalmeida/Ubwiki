462b539dbdbd2ab27ae53f11b65c916bc2c6661a
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-29 22:47:20 +0100

    Update verbete.php

--- verbete.php
+++ verbete.php
@@ -653,8 +653,11 @@
 ?>
 
   <script type='text/javascript'>
-    verbete_editor.setContents({"ops":[{"insert":"Teeste de caneta.
-"}]});
+    verbete_editor.setContents([
+      { insert: 'Hello ' },
+      { insert: 'World!', attributes: { bold: true } },
+      { insert: '\n' }
+    ]);
   </script>
 
 </html>
