aa6706843fe1672af9c626133b0f6d21dc1ff3ff
tsalmeida <tsilvaalmeida@gmail.com>
2019-10-30 20:53:29 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -267,12 +267,14 @@ function bottom_page() {
             });
             $('#travar_verbete').click(function(){
               verbete_editor.disable();
+              $('#destravar_verbete').show();
               $('#travar_verbete').hide();
               $('.ql-toolbar:first').hide();
               $('.ql-editor:first').removeClass('ql-editor-active');
             });
             $('#destravar_verbete').click(function(){
               verbete_editor.enable();
+              $('#travar_verbete').show();
               $('#destravar_verbete').hide();
               $('.ql-toolbar:first').show();
               $('.ql-editor:first').addClass('ql-editor-active');
@@ -280,6 +282,7 @@ function bottom_page() {
             $('#travar_anotacao').click(function(){
               anotacao_editor.disable();
               $('#travar_anotacao').hide();
+              $('#destravar_anotacao').show();
               $('.ql-toolbar:last').hide();
               $('.ql-editor:last').removeClass('ql-editor-active');
             });
@@ -287,6 +290,7 @@ function bottom_page() {
             $('#destravar_anotacao').click(function(){
               anotacao_editor.enable();
               $('#destravar_anotacao').hide();
+              $('#travar_anotacao').show();
               $('.ql-toolbar:last').show();
               $('.ql-editor:last').addClass('ql-editor-active');
             });
