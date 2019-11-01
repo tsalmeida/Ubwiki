daa59bb7c5669b5256cf2f1fa54f39ca5122b75b
tsalmeida <tsilvaalmeida@gmail.com>
2019-11-01 10:55:06 +0100

    tamanho login

--- login.php
+++ login.php
@@ -13,9 +13,9 @@
     carregar_navbar('dark');
     standard_jumbotron("Ubwiki", false);
     ?>
-    <div class="container-fluid my-5">
+    <div class="container my-5">
       <div class="row justify-content-center">
-        <div class="col-4 col-sm-12">
+        <div class="col-lg-5 col-sm-12">
           <h1>Login necessário</h1>
           <p>Você não está logado. Para fazê-lo gratuitamente, por favor crie uma conta na <a href='https://www.grupoubique.com.br'>página do Grupo Ubique</a> e siga o link 'Ubwiki' no topo da página ou na sua lista de cursos.</p>
         </div>
