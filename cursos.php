b5070391572921e58193ac46e48355a3b66c7f22
tsalmeida <tsilvaalmeida@gmail.com>
2019-11-30 18:28:20 +0100

    Update cursos.php

--- cursos.php
+++ cursos.php
@@ -44,6 +44,7 @@
 							$template_conteudo .= "<option value='$curso_id' $selected>$curso_sigla: $curso_titulo</option>";
 						}
 						if ($cursos_do_usuario->num_rows > 0) {
+						    $template_conteudo .= "<option disabled>Seus cursos:</option>";
 						    while ($curso_do_usuario = $cursos_do_usuario->fetch_assoc()) {
 						        $curso_do_usuario_id = $curso_do_usuario['id'];
 						        $curso_do_usuario_sigla = $curso_do_usuario['sigla'];
