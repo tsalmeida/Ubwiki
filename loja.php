b7841dc7b9e3ffe14b608ca1c68f73edd75d5c45
tsalmeida <tsilvaalmeida@gmail.com>
2020-01-16 02:26:28 +0100

    Update loja.php

--- loja.php
+++ loja.php
@@ -97,24 +97,6 @@
 									</div>
 								";
 								
-								$produto_titulo = 'Zeitgeist Base';
-								$produto_pagina_id = 123;
-								$produto_apresentacao = 'O zg base constitui a espinha dorsal no que concerne à preparação em história para o Concurso de Admissão à Carreira Diplomática (CACD). Aqui você tem acesso aos nossos três cursos regulares, os quais, com amplíssima carga horária, proporcionarão uma primeira e já profunda varredura dos temas constantes do edital. Através dos nossos cursos de História do Brasil, História Mundial e História da Política Externa Brasileira, o candidato poderá sistematizar todo o conteúdo recorrentemente cobrado no exame. Trata-se de um primeiro e muito significativo passo rumo à sua aprovação.';
-								$produto_imagem = 'https://ava.ensinabox.com/uploadsTrilha/804ec17b4c70af93f7fc735402e5918b.png';
-								$template_conteudo .= include 'templates/produto.php';
-								
-								$produto_titulo = 'Zeitgeist+';
-								$produto_pagina_id = 123;
-								$produto_apresentacao = 'O ZG+ foi criado com o objetivo de funcionar como uma espécie de "Netflix" para os aspirantes à carreira diplomática. Trata-se de uma assinatura mensal que permite o acesso do candidato a vários cursos de aprofundamento e, também, a um curso de exercícios de História Mundial e de História do Brasil voltado para a primeira fase do CACD, o Teste de Pré-Seleção (TPS). O ZG+ está em constante transformação. Todos os meses oferecemos cursos novos e novas folhinhas de exercícios. Trata-se, portanto, de uma plataforma que constantemente agrega novo repertório aos seus assinantes, proporcionando importantes diferenciais em um concurso cada vez mais competitivo. Você pode entrar e sair do ZG+ quando quiser. Caso não queira mais acessar a plataforma, basta interromper a assinatura.';
-								$produto_imagem = 'https://ava.ensinabox.com/uploadsTrilha/26734d23e7a26e2152f2d7d5e175c4fb.png';
-								$template_conteudo .= include 'templates/produto.php';
-								
-								$produto_titulo = 'Segunda Etapa';
-								$produto_pagina_id = 123;
-								$produto_apresentacao = 'O curso preparatório para a segunda fase do Concurso de Admissão à Carreira Diplomática (CACD) é oferecido àqueles alunos que já estão suficientemente familiarizados com o conteúdo de História do Brasil constante no edital. Neste sentido, é altamente recomendável, para fazer parte do grupo, que o candidato tenha concluído qualquer curso teórico de HB para o CACD oferecido no mercado. No sentido de transcender uma tradição “etapista” dos estudos para o Rio Branco, a nossa intenção é propor uma preparação contínua para a segunda fase do concurso, tornando o formato da prova familiar ao aluno e, através dos temas propostos e das aulas expositivas, adensando o seu repertório, permitindo, assim, maior desenvoltura quando da realização da prova.';
-								$produto_imagem = 'https://ava.ensinabox.com/uploadsTrilha/26c4ddcab81e6fad49b89bec0bb22450.png';
-								$template_conteudo .= include 'templates/produto.php';
-								
 								include 'templates/page_element.php';
 							?>
             </div>
