ecb6c616d86b55f2722cd452f542682d5cc5667b
tsalmeida <tsilvaalmeida@gmail.com>
2020-01-19 17:30:30 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,9 @@
 <?php
 	$pagina_tipo = 'logout';
+	error_log('engine ser aincluido no logout.php');
 	include 'engine.php';
+	error_log('engine foi incluido no logout.php');
 	session_unset();
 	session_destroy();
+	error_log('redirected to login php from logout php');
 	header('Location:login.php');
