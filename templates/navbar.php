da2bd44b5f551ecbfe80bd4dd01da6f9823385db
tsalmeida <tsilvaalmeida@gmail.com>
2020-02-15 13:17:27 +0100

    Update navbar.php

--- templates/navbar.php
+++ templates/navbar.php
@@ -37,7 +37,7 @@
 		        <i class='fas fa-2x $navbar_avatar fa-lg fa-fw'></i>
 		        </a>
 		        <div class='dropdown-menu dropdown-menu-right z-depth-0'>
-		        	<a class='dropdown-item navlink z-depth-0' href='ubwiki.php'><i class='fad fa-door-open fa-fw'></i> Ambientes</a>
+		        	<a class='dropdown-item navlink z-depth-0' href='ubwiki.php'><i class='fad fa-portal-enter fa-fw'></i> Ambientes</a>
 		          <a class='dropdown-item navlink z-depth-0' href='escritorio.php'><i class='fad fa-lamp-desk fa-fw'></i> Seu escritório</a>";
 		if ($carregar_carrinho == true) {
 			echo "<a class='dropdown-item navlink z-depth-0' href='carrinho.php'><i class='fad fa-shopping-cart fa-fw'></i> Seu carrinho</a>";
