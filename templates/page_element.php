87825d567c7d0829b889e58d9cfbad901f4375a3
tsalmeida <tsilvaalmeida@gmail.com>
2020-02-21 23:24:22 +0100

    Update page_element.php

--- templates/page_element.php
+++ templates/page_element.php
@@ -25,7 +25,7 @@
 		$template_col_value = 'col-12';
 	}
 	if (!isset($template_background)) {
-		$template_background = 'rgba-white-strong border border-light';
+		$template_background = 'bg-white border border-light';
 	}
 	
 	if (!isset($template_no_spacer)) {
