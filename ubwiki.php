bad58d0d2ba6f22ac1873f762329cb5656a15af0
tsalmeida <tsilvaalmeida@gmail.com>
2020-03-08 14:25:57 -0300

    changes

--- ubwiki.php
+++ ubwiki.php
@@ -23,6 +23,7 @@
 			echo "<a data-toggle='modal' data-target='#modal_languages' class='text-primary mx-1'><i class='fad fa-language fa-fw fa-2x'></i></a>";
 		}
 		if ($user_id == false) {
+		    echo "<a href='#apresentacao' class='text-success ml-1'><i class='fad fa-portal-enter fa-fw fa-2x'></i></a>";
 			echo "<a data-toggle='modal' data-target='#modal_login' class='text-primary mx-1'><i class='fad fa-user-circle fa-fw fa-2x'></i></a>";
 		}
 		echo "</div>";
