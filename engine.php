97e6ccf8f6fd39d8578f881b14f41182e4616bd3
tsalmeida <tsilvaalmeida@gmail.com>
2020-03-11 09:47:07 -0300

    Update engine.php

--- engine.php
+++ engine.php
@@ -3071,7 +3071,8 @@
 	function send_nova_senha($email, $confirmacao)
 	{
 		$msg = "Sua senha na Ubwiki foi alterada.\nCaso você não tenha conta na Ubwiki, uma nova conta terá sido criada para seu endereço de email.\nPara ativá-la, siga este link:\nhttps://www.ubwiki.com.br/ubwiki/login.php?confirmacao=$confirmacao";
-		mail($email, 'Nova senha na Ubwiki', $msg, null, '-fwebmaster@ubwiki.com.br');
+		$check = mail($email, 'Nova senha na Ubwiki', $msg, null, '-fwebmaster@ubwiki.com.br');
+		error_log($check);
 	}
 	
 	if (isset($_POST['nova_senha'])) {
