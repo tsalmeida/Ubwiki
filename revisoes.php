af3914fb7c275a3e6af541fefeae299273a0d8c8
tsalmeida <tsilvaalmeida@gmail.com>
2020-05-24 20:03:42 +0100

    Update revisoes.php

--- revisoes.php
+++ revisoes.php
@@ -24,16 +24,28 @@
               $template_id = 'revisoes_disponiveis';
               $template_titulo = 'Revisões disponíveis';
               $template_conteudo = false;
-              $orders = $conn->query("SELECT pagina_id, comments, user_id FROM Orders WHERE tipo = 'review' and estado = 1");
+              $orders = $conn->query("SELECT * FROM Orders WHERE tipo = 'review' and estado = 1");
               if ($orders->num_rows > 0) {
                   while ($order = $orders->fetch_assoc()) {
 	                  $template_conteudo .= "<ul class='list-group list-group-flush border p-3 my-3 primary-color rounded'>";
 	                  $order_pagina_id = $order['pagina_id'];
                       $order_comments = $order['comments'];
                       $order_user_id = $order['user_id'];
+                      $order_option1 = $order['option1'];
+                      $order_option2 = $order['option2'];
+                      $order_option3 = $order['option3'];
+                      $order_option4 = $order['option4'];
+                      $order_option5 = $order['option5'];
+                      $order_option6 = $order['option6'];
                       $order_user_apelido = return_apelido_user_id($order_user_id);
                       $order_user_avatar = return_avatar($order_user_id);
                       $template_conteudo .= "<li class='list-group-item d-flex justify-content-center'><span><span class='{$order_user_avatar[1]}'><i class='fad {$order_user_avatar[0]} fa-fw fa-2x'></i></span> <strong>$order_user_apelido</strong></span></li>";
+                      $template_conteudo .= "<li class='list-group-item list-group-item-light'>$order_option1</li>";
+                      $template_conteudo .= "<li class='list-group-item list-group-item-light'>$order_option2</li>";
+                      $template_conteudo .= "<li class='list-group-item list-group-item-light'>$order_option3</li>";
+                      $template_conteudo .= "<li class='list-group-item list-group-item-light'>$order_option4</li>";
+                      $template_conteudo .= "<li class='list-group-item list-group-item-light'>$order_option5</li>";
+                      $template_conteudo .= "<li class='list-group-item list-group-item-light'>$order_option6</li>";
                       $template_conteudo .= "<li class='list-group-item'>$order_comments</li>";
                       $template_conteudo .= return_list_item($order_pagina_id);
 	                  $template_conteudo .= "</ul>";
