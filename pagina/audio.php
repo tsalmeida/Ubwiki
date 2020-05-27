52349b5f24b84c64ae084ca4d4d398cfc4d9add1
tsalmeida <tsilvaalmeida@gmail.com>
2020-05-28 00:27:19 +0100

    Update audio.php

--- pagina/audio.php
+++ pagina/audio.php
@@ -1,14 +1,14 @@
 <?php
-	$pagina_elementos = $conn->query("SELECT DISTINCT elemento_id FROM Paginas_elementos WHERE pagina_id = $pagina_id AND tipo = 'album_musica' AND estado = 1");
-	if ($pagina_elementos->num_rows > 0) {
+	$pagina_audios = $conn->query("SELECT DISTINCT elemento_id FROM Paginas_elementos WHERE pagina_id = $pagina_id AND tipo = 'album_musica' AND estado = 1");
+	if ($pagina_audios->num_rows > 0) {
 		$template_id = 'audios';
 		$template_titulo = $pagina_translated['Áudio'];
 		$template_conteudo = false;
 		$template_conteudo .= "<ul class='list-group list-group-flush'>";
-		while ($pagina_elemento = $pagina_elementos->fetch_assoc()) {
-			$pagina_elemento_id = $pagina_elemento['elemento_id'];
-			$pagina_elemento_pagina_id = return_pagina_id($pagina_elemento_id, 'elemento');
-			$template_conteudo .= return_list_item($pagina_elemento_pagina_id);
+		while ($pagina_audio = $pagina_audios->fetch_assoc()) {
+			$pagina_audio_id = $pagina_audio['elemento_id'];
+			$pagina_audio_pagina_id = return_pagina_id($pagina_audio_id, 'elemento');
+			$template_conteudo .= return_list_item($pagina_audio_pagina_id);
 		}
 		$template_conteudo .= "</ul>";
 		include 'templates/page_element.php';
