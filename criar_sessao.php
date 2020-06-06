2edbe5f729c08d8a2635312a5f2a89eccdcc57bc
tsalmeida <tsilvaalmeida@gmail.com>
2020-06-06 21:01:16 +0100

    Update criar_sessao.php

--- criar_sessao.php
+++ criar_sessao.php
@@ -8,12 +8,12 @@
 	$sessionpath .= '/../sessions/';
 	session_save_path($sessionpath);
 
-	if (session_status() == PHP_SESSION_NONE) {
+	if (session_status() == 1) {
 		error_log('first php session none:');
 		session_start();
 		$_SESSION['user_info'] = 'visitante';
-		error_log('session was started inside the PHP_SESSION_NONE');
-		error_log("user_info dentro do session_status: {$_SESSION['user_info']}");
+		error_log('session was started inside the PHP_SESSION_NONE. session status:');
+		error_log(session_status());
 	}
 
 	error_log("user_info fora do session_status: {$_SESSION['user_info']}");
@@ -24,21 +24,21 @@
 		error_log($_SESSION['user_info']);
 	}
 
-	if (session_status() == PHP_SESSION_NONE) {
+	if (session_status() == 1) {
 		error_log('second php session none:');
-		//session_start();
+		session_start();
 		$_SESSION['user_info'] = 'visitante';
-		error_log('second session was started inside the PHP_SESSION_NONE');
-		error_log("user_info dentro do segundo session_status: {$_SESSION['user_info']}");
+		error_log('second session was started inside the PHP_SESSION_NONE. Session status de novo:');
+		error_log(session_status());
 	}
 
 	if (!isset($_SESSION['user_info'])) {
 		$_SESSION['user_info'] = 'visitante';
 	}
 
-	error_log("user_info fora do session_status: {$_SESSION['user_info']}");
-
-	//session_start();
+	error_log("E agora session_start declared again, followed by another session_status check:");
+	session_start();
+	error_log(session_status());
 
 	if (!isset($_SESSION['user_info'])) {
 		$_SESSION['user_info'] = 'visitante';
