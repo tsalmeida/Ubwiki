e3f48523dcaef920d6879cd1546a0fbeb21d5ff9
tsalmeida <tsilvaalmeida@gmail.com>
2020-06-06 15:37:10 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -6,8 +6,15 @@
 		session_save_path($sessionpath);
 		session_start();
 		$_SESSION['user_info'] = 'visitante';
-	} else {
-		session_start();
+	}
+
+	$sessionpath = getcwd();
+	$sessionpath .= '/../sessions';
+	session_save_path($sessionpath);
+	session_start();
+
+	if (!isset($_SESSION['user_info'])) {
+		$_SESSION['user_info'] = 'visitante';
 	}
 
 	if (!isset($pagina_tipo)) {
