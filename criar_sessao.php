88a500ffc8f9f33202f5238f7dbb189b0a1acb34
tsalmeida <tsilvaalmeida@gmail.com>
2020-06-06 17:24:15 +0100

    Update criar_sessao.php

--- criar_sessao.php
+++ criar_sessao.php
@@ -1,18 +1,15 @@
 <?php
 
 	$sessionpath = getcwd();
-	$sessionpath .= '/sessions/';
-	error_log($sessionpath);
+	$sessionpath .= '/../sessions/';
+
 	session_save_path($sessionpath);
 	session_start();
-	error_log($pagina_tipo);
+
 	if (session_status() == PHP_SESSION_NONE) {
-		$sessionpath = getcwd();
-		$sessionpath .= '/sessions/';
 		session_save_path($sessionpath);
 		session_start();
 		$_SESSION['user_info'] = 'visitante';
-		error_log("SESSAO CRIADA EM $pagina_tipo");
 	}
 
 	if (!isset($_SESSION['user_info'])) {
