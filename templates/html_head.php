23f7e4f49dac850c224552ca2270c22fd4fbfd9b
tsalmeida <tsilvaalmeida@gmail.com>
2020-07-06 15:08:02 +0100

    changes

--- templates/html_head.php
+++ templates/html_head.php
@@ -27,7 +27,7 @@
         <!-- Material Design Bootstrap -->
         <link href='css/mdb.min.css' rel='stylesheet'>
         <!-- Your custom styles (optional) -->
-        <link href='css/style.css?20200703' rel='stylesheet'>
+        <link href='css/style.css?20200706' rel='stylesheet'>
         <!-- Bootstrap Horizon -->
         <link href='css/bootstrap-horizon.css' rel='stylesheet'>
         <link type='image/vnd.microsoft.icon' rel='icon' href='../imagens/favicon.ico'/>
