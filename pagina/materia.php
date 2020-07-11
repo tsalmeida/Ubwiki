cb544ea74917c63f4d7dadbd879da66c3fc8e625
tsalmeida <tsilvaalmeida@gmail.com>
2020-07-11 15:57:04 +0100

    Update materia.php

--- pagina/materia.php
+++ pagina/materia.php
@@ -66,6 +66,9 @@
 				while ($subtopico = $subtopicos->fetch_assoc()) {
 					$subtopico_pagina_id = $subtopico['elemento_id'];
 					$subtopico_pagina_info = return_pagina_info($subtopico_pagina_id, true);
+					if ($subtopico_pagina_info == false) {
+						continue;
+					}
 					$subtopico_pagina_estado = $subtopico_pagina_info[3];
 					$subtopico_pagina_titulo = $subtopico_pagina_info[6];
 					$topico_completo = false;
@@ -91,6 +94,10 @@
 						while ($subsubtopico = $subsubtopicos->fetch_assoc()) {
 							$subsubtopico_pagina_id = $subsubtopico['elemento_id'];
 							$subsubtopico_pagina_info = return_pagina_info($subsubtopico_pagina_id, true);
+							if ($subsubtopico_pagina_info == false) {
+								continue;
+							}
+
 							$subsubtopico_pagina_estado = $subsubtopico_pagina_info[3];
 							$subsubtopico_pagina_titulo = $subsubtopico_pagina_info[6];
 							$topico_completo = false;
@@ -115,6 +122,9 @@
 								while ($subsubsubtopico = $subsubsubtopicos->fetch_assoc()) {
 									$subsubsubtopico_pagina_id = $subsubsubtopico['elemento_id'];
 									$subsubsubtopico_pagina_info = return_pagina_info($subsubsubtopico_pagina_id, true);
+									if ($subsubsubtopico_pagina_info == false) {
+										continue;
+									}
 									$subsubsubtopico_pagina_estado = $subsubsubtopico_pagina_info[3];
 									$subsubsubtopico_pagina_titulo = $subsubsubtopico_pagina_info[6];
 									$topico_completo = false;
@@ -140,6 +150,13 @@
 										while ($subsubsubsubtopico = $subsubsubsubtopicos->fetch_assoc()) {
 											$subsubsubsubtopico_pagina_id = $subsubsubsubtopico['elemento_id'];
 											$subsubsubsubtopico_pagina_info = return_pagina_info($subsubsubsubtopico_pagina_id, true);
+											if ($subsubsubsubtopico_pagina_info == false) {
+												continue;
+											}
+
+											if ($subsubsubsubtopico_pagina_info == false) {
+												continue;
+											}
 											$subsubsubsubtopico_pagina_estado = $subsubsubsubtopico_pagina_info[3];
 											$subsubsubsubtopico_pagina_titulo = $subsubsubsubtopico_pagina_info[6];
 											$topico_completo = false;
